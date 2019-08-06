bedbab3bd8f2e7658bf3fccfb96ee0d774bb9a4d
Simon Adcock <simonadcock2@gmail.com>
2019-08-06 12:11:40 +0100

    add appearances knob

--- packages/button/stories.js
+++ packages/button/stories.js
@@ -1,9 +1,24 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { Button } from "./button";
+import { withKnobs, text, radios } from "@storybook/addon-knobs";
+import { Button, appearances } from "./button";
 
-storiesOf("Button", module)
-	.add("with text", () => <Button>Hello Button</Button>)
+const stories = storiesOf("Button", module);
+
+const appearanceKnobs = appearances.reduce(
+	(acc, curr) => Object.assign({}, acc, { [curr]: curr }),
+	{}
+);
+
+const getAppearance = () => radios("Appearance", appearanceKnobs, "primary");
+
+stories.addDecorator(withKnobs);
+stories
+	.add("with text", () => (
+		<Button appearance={getAppearance()}>
+			{text("Button Label", "Button Label")}
+		</Button>
+	))
 	.add("with emoji", () => (
 		<Button>
 			<span role="img" aria-label="so cool">
