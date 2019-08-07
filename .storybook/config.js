7cdb61df6a5d335ea311a061a93c08e63c080b00
Simon Adcock <simonadcock2@gmail.com>
2019-08-07 16:46:52 +0100

    add foundations to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,7 @@
 import { configure } from "@storybook/react";
 
 function loadStories() {
+	require("../packages/foundations/stories.js");
 	require("../packages/button/stories.js");
 	require("../packages/dropdown/stories.js");
 	require("../packages/radio/stories.js");
