123359750a6eb49d306259f24b647a18c8bd0b6b
Simon Adcock <simonadcock2@gmail.com>
2019-08-20 12:42:03 +0100

    remove foundation stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from "@storybook/react";
 
 function loadStories() {
-	require("../packages/foundations/stories.jsx");
 	require("../packages/button/stories.jsx");
 	require("../packages/radio/stories.jsx");
 }
