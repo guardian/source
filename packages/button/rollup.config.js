ad78ecbe5cea559c910ccbd92491a04e32b9b793
Simon Adcock <simonadcock2@gmail.com>
2019-08-20 16:27:04 +0100

    better builds

--- packages/button/rollup.config.js
+++ packages/button/rollup.config.js
@@ -2,14 +2,20 @@ import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const extensions = [".ts", ".tsx"];
 
 module.exports = {
 	input: "button.tsx",
-	output: {
-		file: "dist/button.js",
-		format: "cjs"
-	},
+	output: [
+		{
+			file: "dist/button.js",
+			format: "cjs"
+		},
+		{
+			file: "dist/button.esm.js",
+			format: "esm"
+		}
+	],
 	external: ["react", "@emotion/core", "@emotion/css"],
 	plugins: [babel({ extensions }), resolve({ extensions }), commonjs()]
 };
