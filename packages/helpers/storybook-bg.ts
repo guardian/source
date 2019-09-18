a186f42bba310410909f0b7241ed3c2705688b36
Simon Adcock <simonadcock2@gmail.com>
2019-09-18 16:49:34 +0100

    add yellow background

--- packages/helpers/storybook-bg.ts
+++ packages/helpers/storybook-bg.ts
@@ -2,8 +2,9 @@ import { palette } from "@guardian/src-foundations"
 
 const storybookBackgrounds = {
 	light: { name: "light", value: palette.neutral[100] },
-	blue: { name: "blue", value: palette.brand.main },
 	dark: { name: "dark", value: palette.neutral[10] },
+	blue: { name: "blue", value: palette.brand.main },
+	yellow: { name: "yellow", value: palette.yellow.main },
 }
 
 Object.freeze(storybookBackgrounds)
