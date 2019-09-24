b91960cbd1129106293a7bace839464baafcf4da
Simon Adcock <simonadcock2@gmail.com>
2019-09-24 16:54:04 +0100

    add close icon

---
+++ packages/svgs/close.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const SvgClose = () => (
+	<svg
+		className="svg-close"
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="-2 -2 38 38"
+		preserveAspectRatio="xMinYMid"
+	>
+		<path
+			d="M16.525 19.87L30.865 33 33 30.87 19.9 16.5 33 2.13 30.866 0l-14.34 13.13L2.133.05 0 2.18 13.15 16.5 0 30.82l2.134 2.13"
+			fillRule="evenodd"
+		/>
+	</svg>
+)
