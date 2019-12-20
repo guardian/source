9a0875a6af72a5ad73f4e9e8d2a4a370a3d1e36a
Simon Adcock <simonadcock2@gmail.com>
2019-12-20 16:07:04 +0000

    expose all components by theme

--- packages/foundations/src/themes/index.ts
+++ packages/foundations/src/themes/index.ts
@@ -2,3 +2,39 @@ export * from "./button"
 export * from "./inline-error"
 export * from "./radio"
 export * from "./text-input"
+
+import {
+	buttonLight,
+	buttonBrand,
+	buttonBrandYellow,
+	buttonReaderRevenue,
+	buttonReaderRevenueYellow,
+} from "./button"
+import { inlineErrorLight, inlineErrorBrand } from "./inline-error"
+import { radioLight, radioBrand } from "./radio"
+import { textInputLight } from "./text-input"
+
+export const light = {
+	...buttonLight,
+	...inlineErrorLight,
+	...radioLight,
+	...textInputLight,
+}
+
+export const brand = {
+	...buttonBrand,
+	...inlineErrorBrand,
+	...radioBrand,
+}
+
+export const brandYellow = {
+	...buttonBrandYellow,
+}
+
+export const readerRevenue = {
+	...buttonReaderRevenue,
+}
+
+export const readerRevenueYellow = {
+	...buttonReaderRevenueYellow,
+}
