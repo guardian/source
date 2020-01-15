832bc58ae6f8a25b050c499a64bf92c2ae60cd68
Simon Adcock <simonadcock2@gmail.com>
2020-01-15 15:29:12 +0000

    rename cta colors to button

--- packages/foundations/src/themes/button.ts
+++ packages/foundations/src/themes/button.ts
@@ -14,40 +14,41 @@ export type ButtonTheme = {
 
 export const buttonLight: { button: ButtonTheme } = {
 	button: {
-		textPrimary: palette.text.ctaPrimary,
-		backgroundPrimary: palette.background.ctaPrimary,
-		backgroundPrimaryHover: palette.background.ctaPrimaryHover,
-		textSecondary: palette.text.ctaSecondary,
-		backgroundSecondary: palette.background.ctaSecondary,
-		backgroundSecondaryHover: palette.background.ctaSecondaryHover,
-		textTertiary: palette.text.ctaSecondary,
+		textPrimary: palette.text.buttonPrimary,
+		backgroundPrimary: palette.background.buttonPrimary,
+		backgroundPrimaryHover: palette.background.buttonPrimaryHover,
+		textSecondary: palette.text.buttonSecondary,
+		backgroundSecondary: palette.background.buttonSecondary,
+		backgroundSecondaryHover: palette.background.buttonSecondaryHover,
+		textTertiary: palette.text.buttonSecondary,
 		backgroundTertiary: palette.background.primary,
 	},
 }
 
 export const buttonBrand: { button: ButtonTheme } = {
 	button: {
-		textPrimary: palette.text.brand.ctaPrimary,
-		backgroundPrimary: palette.background.brand.ctaPrimary,
-		backgroundPrimaryHover: palette.background.brand.ctaPrimaryHover,
-		textSecondary: palette.text.brand.ctaSecondary,
-		backgroundSecondary: palette.background.brand.ctaSecondary,
-		backgroundSecondaryHover: palette.background.brand.ctaSecondaryHover,
-		textTertiary: palette.text.brand.ctaSecondary,
+		textPrimary: palette.text.brand.buttonPrimary,
+		backgroundPrimary: palette.background.brand.buttonPrimary,
+		backgroundPrimaryHover: palette.background.brand.buttonPrimaryHover,
+		textSecondary: palette.text.brand.buttonSecondary,
+		backgroundSecondary: palette.background.brand.buttonSecondary,
+		backgroundSecondaryHover: palette.background.brand.buttonSecondaryHover,
+		textTertiary: palette.text.brand.buttonSecondary,
 		backgroundTertiary: palette.background.brand.primary,
 	},
 }
 
 export const buttonBrandYellow: { button: ButtonTheme } = {
 	button: {
-		textPrimary: palette.text.brandYellow.ctaPrimary,
-		backgroundPrimary: palette.background.brandYellow.ctaPrimary,
-		backgroundPrimaryHover: palette.background.brandYellow.ctaPrimaryHover,
-		textSecondary: palette.text.brandYellow.ctaSecondary,
-		backgroundSecondary: palette.background.brandYellow.ctaSecondary,
+		textPrimary: palette.text.brandYellow.buttonPrimary,
+		backgroundPrimary: palette.background.brandYellow.buttonPrimary,
+		backgroundPrimaryHover:
+			palette.background.brandYellow.buttonPrimaryHover,
+		textSecondary: palette.text.brandYellow.buttonSecondary,
+		backgroundSecondary: palette.background.brandYellow.buttonSecondary,
 		backgroundSecondaryHover:
-			palette.background.brandYellow.ctaSecondaryHover,
-		textTertiary: palette.text.brandYellow.ctaSecondary,
+			palette.background.brandYellow.buttonSecondaryHover,
+		textTertiary: palette.text.brandYellow.buttonSecondary,
 		backgroundTertiary: palette.background.brandYellow.primary,
 	},
 }
