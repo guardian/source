1b310ceb2941473f742a4b36803f93bd4934c70a
Simon Adcock <simonadcock2@gmail.com>
2020-01-16 15:46:06 +0000

    extract global and functional colours into modules

---
+++ packages/foundations/src/palette/border.ts
@@ -0,0 +1,26 @@
+import { neutral, error, brand, sport } from "./global"
+
+export const border = {
+	primary: neutral[60],
+	secondary: neutral[86],
+	error: error[400],
+	focusHalo: sport[500],
+	radio: neutral[60],
+	radioHover: brand[500],
+	radioError: error[400],
+	textInput: neutral[60],
+	checkbox: neutral[60],
+	checkboxHover: brand[500],
+	checkboxChecked: brand[500],
+	checkboxError: error[400],
+	brand: {
+		error: error[500],
+		radio: brand[800],
+		radioHover: neutral[100],
+		radioError: error[500],
+		checkbox: brand[800],
+		checkboxHover: neutral[100],
+		checkboxChecked: neutral[100],
+		checkboxError: error[500],
+	},
+}
---
+++ packages/foundations/src/palette/line.ts
@@ -0,0 +1,11 @@
+import { neutral, brand } from "./global"
+
+export const line = {
+	primary: neutral[86],
+	brand: {
+		primary: brand[600],
+	},
+	brandYellow: {
+		primary: neutral[7],
+	},
+}
