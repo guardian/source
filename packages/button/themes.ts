60fd4bcb3abad1879d96760ed8fabf1f5cbe5ce9
Simon Adcock <simonadcock2@gmail.com>
2020-01-20 12:56:34 +0000

    use individually exported global colours

--- packages/button/themes.ts
+++ packages/button/themes.ts
@@ -1,42 +1,42 @@
-import { palette } from "@guardian/src-foundations"
+import { neutral, brand, brandYellow } from "@guardian/src-foundations/palette"
 import { ButtonTheme } from "@guardian/src-foundations/themes"
 
 const text = {
 	readerRevenue: {
-		primary: palette.neutral[100],
-		secondary: palette.neutral[60],
-		ctaPrimary: palette.brand[400],
-		ctaSecondary: palette.brandYellow[400],
+		primary: neutral[100],
+		secondary: neutral[60],
+		ctaPrimary: brand[400],
+		ctaSecondary: brandYellow[400],
 	},
 	readerRevenueAlt: {
-		primary: palette.neutral[7],
-		secondary: palette.neutral[60],
-		ctaPrimary: palette.neutral[100],
-		ctaSecondary: palette.neutral[7],
+		primary: neutral[7],
+		secondary: neutral[60],
+		ctaPrimary: neutral[100],
+		ctaSecondary: neutral[7],
 	},
 }
 const background = {
 	readerRevenue: {
-		primary: palette.brand[400],
-		ctaPrimary: palette.brandYellow[400],
-		ctaPrimaryHover: palette.brandYellow[300],
-		ctaSecondary: palette.brand[400],
+		primary: brand[400],
+		ctaPrimary: brandYellow[400],
+		ctaPrimaryHover: brandYellow[300],
+		ctaSecondary: brand[400],
 		ctaSecondaryHover: "#234B8A",
 	},
 	readerRevenueAlt: {
-		primary: palette.brandYellow[400],
-		ctaPrimary: palette.neutral[7],
+		primary: brandYellow[400],
+		ctaPrimary: neutral[7],
 		ctaPrimaryHover: "#454545",
-		ctaSecondary: palette.brandYellow[400],
-		ctaSecondaryHover: palette.brandYellow[300],
+		ctaSecondary: brandYellow[400],
+		ctaSecondaryHover: brandYellow[300],
 	},
 }
 const border = {
 	readerRevenue: {
-		ctaSecondary: palette.brandYellow[400],
+		ctaSecondary: brandYellow[400],
 	},
 	readerRevenueAlt: {
-		ctaSecondary: palette.neutral[7],
+		ctaSecondary: neutral[7],
 	},
 }
 
