b7e58554697338f8766d9e69bd7767ce702f5051
Simon Adcock <simonadcock2@gmail.com>
2020-01-24 14:12:38 +0000

    explicitly build palette folder

--- packages/foundations/rollup.config.js
+++ packages/foundations/rollup.config.js
@@ -4,19 +4,19 @@ import resolve from "rollup-plugin-node-resolve"
 const extensions = [".ts", ".tsx"]
 const plugins = [babel({ extensions }), resolve({ extensions })]
 
-// NOTE: the palette folder (not included here) is built as a side effect of the
-// palette being exposed by src/index.ts
-const folders = ["accessibility", "mq", "themes", "typography"].map(folder => ({
-	input: `src/${folder}/index.ts`,
-	output: [
-		{
-			file: `${folder}/index.js`,
-			format: "cjs",
-		},
-	],
-	plugins,
-	external: ["@guardian/src-foundations"],
-}))
+const folders = ["accessibility", "mq", "palette", "themes", "typography"].map(
+	folder => ({
+		input: `src/${folder}/index.ts`,
+		output: [
+			{
+				file: `${folder}/index.js`,
+				format: "cjs",
+			},
+		],
+		plugins,
+		external: ["@guardian/src-foundations"],
+	}),
+)
 
 module.exports = [
 	{
