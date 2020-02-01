aba495d3479d10f38c3727550c78f063d2b7240a
SiAdcock <simonadcock2@gmail.com>
2020-02-01 21:59:38 +0000

    add mq until test

--- src/core/foundations/src/mq/mq.test.ts
+++ src/core/foundations/src/mq/mq.test.ts
@@ -1,5 +1,11 @@
-import { breakpoints, from } from "./index"
+import { breakpoints, from, until } from "./index"
 
 it("from should return a min-width media query", () => {
 	expect(from.mobile).toBe(`@media (min-width: ${`${breakpoints.mobile}px`})`)
+})
+
+it("until should return a max-width media query", () => {
+	expect(until.wide).toBe(
+		`@media (max-width: ${`${breakpoints.wide - 1}px`})`,
+	)
 })
