2a7770695f7a7997709bee0d740b338c15ab47e2
SiAdcock <simonadcock2@gmail.com>
2020-02-07 21:58:04 +0000

    update stylelint plugin

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,9 +2,9 @@ module.exports = {
 	processors: ["stylelint-processor-styled-components"],
 	extends: [
 		"stylelint-config-recommended",
-		"stylelint-config-styled-components"
+		"stylelint-config-styled-components",
 	],
 	rules: {
-		"no-descending-specificity": null
-	}
-};
+		"no-descending-specificity": null,
+	},
+}
