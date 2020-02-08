acd4988e268d3a52cc1cc1fcc7591fd61af9d3fe
SiAdcock <simonadcock2@gmail.com>
2020-02-08 23:26:21 +0000

    support ungrouped radio buttons

---
+++ src/core/components/radio/stories/ungrouped.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { Radio, radioDefault } from "../index"
+import { ThemeProvider } from "emotion-theming"
+
+/* eslint-disable react/jsx-key */
+const radios = [
+	<Radio value="red" label="Red" name="colours" />,
+	<Radio value="green" label="Green" name="colours" checked={true} />,
+	<Radio value="blue" label="Blue" name="colours" />,
+]
+/* eslint-enable react/jsx-key */
+
+const ungrouped = () => (
+	<ThemeProvider theme={radioDefault}>
+		{radios.map((radio, index) =>
+			React.cloneElement(radio, { key: index }),
+		)}
+	</ThemeProvider>
+)
+
+ungrouped.story = {
+	name: "ungrouped",
+}
+
+export { ungrouped }
