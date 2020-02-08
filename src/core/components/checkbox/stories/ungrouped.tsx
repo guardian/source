7a7a008f8572da9d416a21b7fd52c3cd04c87665
SiAdcock <simonadcock2@gmail.com>
2020-02-08 22:33:13 +0000

    allow checkboxes to exist outside a checkboxgroup

---
+++ src/core/components/checkbox/stories/ungrouped.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import { ThemeProvider } from "emotion-theming"
+
+import { Checkbox, checkboxDefault } from "../index"
+
+/* eslint-disable react/jsx-key */
+const checkboxes = [
+	<Checkbox label="Guardian Today: UK" value="today_uk" name="emails" />,
+	<Checkbox label="Guardian Today: US" value="today_us" name="emails" />,
+]
+/* eslint-enable react/jsx-key */
+
+const ungrouped = () => (
+	<ThemeProvider theme={checkboxDefault}>
+		{checkboxes.map((checkbox, index) =>
+			React.cloneElement(checkbox, { key: index }),
+		)}
+	</ThemeProvider>
+)
+
+ungrouped.story = {
+	name: "ungrouped",
+}
+
+export { ungrouped }
