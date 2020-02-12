3aeadf067d6aa9d8bacafe0b85cd8161130676c0
Simon Adcock <simonadcock2@gmail.com>
2020-02-12 15:52:10 +0000

    add choice card first pass

---
+++ src/core/components/choice-card/index.tsx
@@ -0,0 +1,88 @@
+import React, { ReactNode } from "react"
+import { fieldset, input, choiceCard, errorChoiceCard } from "./styles"
+import { InlineError } from "@guardian/src-inline-error"
+export { choiceCardDefault } from "@guardian/src-foundations/themes"
+
+const ChoiceCardGroup = ({
+	name,
+	error,
+	children,
+	...props
+}: {
+	name: string
+	error?: string
+	children: JSX.Element | JSX.Element[]
+}) => {
+	// TODO: This is currently a div instead of a fieldset due to a Chrome / Safari
+	// bug that prevents flexbox model working on fieldset elements
+	// https://bugs.chromium.org/p/chromium/issues/detail?id=375693
+	return (
+		<div css={[fieldset]} {...props}>
+			{error && <InlineError>{error}</InlineError>}
+			{React.Children.map(children, child => {
+				return React.cloneElement(
+					child,
+					Object.assign(error ? { error: true } : {}, {
+						name,
+					}),
+				)
+			})}
+		</div>
+	)
+}
+
+const ChoiceCard = ({
+	id,
+	label: labelContent,
+	value,
+	supporting,
+	checked,
+	error,
+	...props
+}: {
+	id: string
+	label: ReactNode
+	value: string
+	supporting?: ReactNode
+	checked?: boolean
+	error: boolean
+}) => {
+	const setChoiceCardState = (el: HTMLInputElement | null) => {
+		if (el && checked != null) {
+			el.checked = checked
+		}
+	}
+
+	return (
+		<>
+			<input
+				css={input}
+				id={id}
+				value={value}
+				aria-invalid={error}
+				ref={setChoiceCardState}
+				{...props}
+			/>
+			<label
+				css={theme => [
+					choiceCard(theme.choiceCard && theme),
+					error ? errorChoiceCard(theme.choiceCard && theme) : "",
+				]}
+				htmlFor={id}
+			>
+				{labelContent}
+			</label>
+		</>
+	)
+}
+
+const choiceCardDefaultProps = {
+	disabled: false,
+	type: "radio",
+	defaultChecked: false,
+	error: false,
+}
+
+ChoiceCard.defaultProps = { ...choiceCardDefaultProps }
+
+export { ChoiceCardGroup, ChoiceCard }
---
+++ src/core/components/choice-card/styles.ts
@@ -0,0 +1,62 @@
+import { css } from "@emotion/core"
+import { space, size } from "@guardian/src-foundations"
+import { visuallyHidden } from "@guardian/src-foundations/accessibility"
+import { textSans } from "@guardian/src-foundations/typography"
+import { focusHalo } from "@guardian/src-foundations/accessibility"
+import {
+	ChoiceCardTheme,
+	choiceCardDefault,
+} from "@guardian/src-foundations/themes"
+
+export const fieldset = css`
+	display: flex;
+	justify-content: flex-start;
+	border: 0;
+`
+
+export const input = ({
+	choiceCard,
+}: { choiceCard: ChoiceCardTheme } = choiceCardDefault) => css`
+	${visuallyHidden};
+
+	&:checked + label {
+		color: ${choiceCard.textChecked};
+		border-color: ${choiceCard.borderColorChecked};
+		background-color: ${choiceCard.backgroundChecked};
+	}
+`
+
+export const choiceCard = ({
+	choiceCard,
+}: { choiceCard: ChoiceCardTheme } = choiceCardDefault) => css`
+	flex: 1 0 auto;
+	box-sizing: border-box;
+	display: flex;
+	/* TODO: let's talk about size! */
+	min-height: ${size.large}px;
+	max-width: 150px;
+	margin: 0 ${space[2]}px 0 0;
+	align-items: center;
+	justify-content: center;
+
+	border-width: 2px;
+	border-style: solid;
+	/* TODO: prefer curentColor unless it has to be different to text colour */
+	border-color: ${choiceCard.borderColor};
+	border-radius: 4px;
+	position: relative;
+
+	color: ${choiceCard.text};
+	${textSans.medium({ fontWeight: "bold" })};
+	cursor: pointer;
+
+	&:focus {
+		${focusHalo};
+	}
+`
+
+export const errorChoiceCard = ({
+	choiceCard,
+}: { choiceCard: ChoiceCardTheme } = choiceCardDefault) => css`
+	/* border: 4px solid #ff0000; */
+`
