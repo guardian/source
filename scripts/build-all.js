2d1253dcaaf4f39ac39741efd87d632a1a674ec9
SiAdcock <simonadcock2@gmail.com>
2020-02-17 22:27:50 +0000

    extract getComponentNames utility function

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -1,17 +1,11 @@
-const fs = require("fs")
 const execa = require("execa")
-const paths = require("./paths")
-const { promisify } = require("util")
-
-const readdirP = promisify(fs.readdir)
-const statP = promisify(fs.stat)
+const { paths, getComponentPaths } = require("./paths")
 
 const build = dir => {
 	return execa("yarn", ["--cwd", `${dir}`, "run", "build"], {
 		stdio: "inherit",
 	})
 }
-const isDirectory = path => statP(path).then(stats => stats.isDirectory())
 
 const { foundations, svgs, utilities, components } = paths
 
@@ -23,24 +17,8 @@ const highPriorityPackages = [foundations, svgs, utilities]
 const mediumPriorityPackages = [`${components}/inline-error`]
 
 // not depended on
-const lowPriorityPackages = readdirP(components).then(componentDirs =>
-	Promise.all(
-		componentDirs.map(componentDirName =>
-			isDirectory(`${components}/${componentDirName}`).then(
-				isDirectory => {
-					if (!isDirectory) return Promise.resolve()
-					if (
-						mediumPriorityPackages.includes(
-							`${components}/${componentDirName}`,
-						)
-					)
-						return Promise.resolve()
-
-					return `${components}/${componentDirName}`
-				},
-			),
-		),
-	),
+const lowPriorityPackages = getComponentPaths().then(paths =>
+	paths.filter(path => !mediumPriorityPackages.includes(path)),
 )
 
 Promise.all(highPriorityPackages.map(dir => build(dir)))
--- scripts/paths.js
+++ scripts/paths.js
@@ -1,13 +1,38 @@
 const path = require("path")
+const fs = require("fs")
+const { promisify } = require("util")
+
+const readdirP = promisify(fs.readdir)
+const statP = promisify(fs.stat)
 
 const foundations = path.join(__dirname, "../src/core/foundations")
 const svgs = path.join(__dirname, "../src/core/svgs")
 const utilities = path.join(__dirname, "../src/core/utilities")
 const components = path.join(__dirname, "../src/core/components")
 
-module.exports = {
+const isDirectory = path => statP(path).then(stats => stats.isDirectory())
+
+const getComponentPaths = () =>
+	readdirP(components)
+		.then(componentDirs =>
+			Promise.all(
+				componentDirs.map(componentDirName =>
+					isDirectory(`${components}/${componentDirName}`).then(
+						isDirectory => {
+							if (!isDirectory) return Promise.resolve()
+
+							return `${components}/${componentDirName}`
+						},
+					),
+				),
+			),
+		)
+		.then(paths => Promise.resolve(paths.filter(path => !!path)))
+
+module.exports.paths = {
 	foundations,
 	svgs,
 	utilities,
 	components,
 }
+module.exports.getComponentPaths = getComponentPaths
