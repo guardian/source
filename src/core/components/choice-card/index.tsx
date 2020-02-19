ad3f5f5928b0f5c996e07aff6fc085801c6cd836
Simon Adcock <simonadcock2@gmail.com>
2020-02-19 10:23:12 +0000

    pass classname to choicecard and group

--- src/core/components/choice-card/index.tsx
+++ src/core/components/choice-card/index.tsx
@@ -1,6 +1,15 @@
 import React, { ReactNode } from "react"
 import { fieldset, input, choiceCard } from "./styles"
+import { Props } from "../../../common/props"
+
 export { choiceCardDefault } from "@guardian/src-foundations/themes"
+
+interface ChoiceCardGroupProps extends Props {
+	name: string
+	multi?: boolean
+	error?: string
+	children: JSX.Element | JSX.Element[]
+}
 
 const ChoiceCardGroup = ({
 	name,
@@ -8,12 +17,7 @@ const ChoiceCardGroup = ({
 	error,
 	children,
 	...props
-}: {
-	name: string
-	multi?: boolean
-	error?: string
-	children: JSX.Element | JSX.Element[]
-}) => {
+}: ChoiceCardGroupProps) => {
 	// TODO: This is currently a div instead of a fieldset due to a Chrome / Safari
 	// bug that prevents flexbox model working on fieldset elements
 	// https://bugs.chromium.org/p/chromium/issues/detail?id=375693
@@ -34,6 +38,15 @@ const ChoiceCardGroup = ({
 	)
 }
 
+interface ChoiceCardProps extends Props {
+	id: string
+	label: ReactNode
+	value: string
+	supporting?: ReactNode
+	checked?: boolean
+	error: boolean
+}
+
 const ChoiceCard = ({
 	id,
 	label: labelContent,
@@ -41,14 +54,7 @@ const ChoiceCard = ({
 	checked,
 	error,
 	...props
-}: {
-	id: string
-	label: ReactNode
-	value: string
-	supporting?: ReactNode
-	checked?: boolean
-	error: boolean
-}) => {
+}: ChoiceCardProps) => {
 	const setChoiceCardState = (el: HTMLInputElement | null) => {
 		if (el && checked != null) {
 			el.checked = checked
