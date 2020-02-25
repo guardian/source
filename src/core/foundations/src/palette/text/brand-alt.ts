ed6a142856a7288c8e79b34742473429d801a0c3
Simon Adcock <simonadcock2@gmail.com>
2020-02-25 16:51:11 +0000

    decouple default text colours from globals

---
+++ src/core/foundations/src/palette/text/brand-alt.ts
@@ -0,0 +1,9 @@
+import { neutral } from "../global"
+export const brandAltText = {
+	primary: neutral[7],
+	secondary: neutral[60],
+	buttonPrimary: neutral[100],
+	buttonSecondary: neutral[7],
+	linkPrimary: neutral[7],
+	linkPrimaryHover: neutral[7],
+}
---
+++ src/core/foundations/src/palette/text/brand.ts
@@ -0,0 +1,15 @@
+import { neutral, error, brand } from "../global"
+export const brandText = {
+	primary: neutral[100],
+	secondary: brand[800],
+	error: error[500],
+	buttonPrimary: brand[400],
+	buttonSecondary: neutral[100],
+	checkbox: neutral[100],
+	checkboxSupporting: brand[800],
+	checkboxIndeterminate: brand[800],
+	linkPrimary: neutral[100],
+	linkPrimaryHover: neutral[100],
+	radio: neutral[100],
+	radioSupporting: brand[800],
+}
