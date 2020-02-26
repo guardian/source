ddf986648d011ac629369b6ba9f5299c0e9bab33
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2020-02-26 15:54:31 +0000

    Re-wrote remSpace as an object literal

--- src/core/foundations/src/space.ts
+++ src/core/foundations/src/space.ts
@@ -13,9 +13,17 @@ const space = {
 	24: _space[8],
 }
 
-const remSpace = Object.entries(space).reduce(
-	(acc, [k, v]) => ({ ...acc, [k]: `${v / rootPixelFontSize}rem` }),
-	{} as Record<keyof (typeof space), string>,
-)
+const pxToRem = (px: number): string => `${px / rootPixelFontSize}rem`
+
+const remSpace: { [K in keyof (typeof space)]: string } = {
+	1: pxToRem(space[1]),
+	2: pxToRem(space[2]),
+	3: pxToRem(space[3]),
+	5: pxToRem(space[5]),
+	6: pxToRem(space[6]),
+	9: pxToRem(space[9]),
+	12: pxToRem(space[12]),
+	24: pxToRem(space[24]),
+}
 
 export { space, remSpace }
