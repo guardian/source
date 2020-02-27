d931fec6b041c395b42145c6934a266f574016f6
Simon Adcock <simonadcock2@gmail.com>
2020-02-27 15:57:02 +0000

    decouple brand text colours from globalse

--- src/core/foundations/src/palette/text/brand.ts
+++ src/core/foundations/src/palette/text/brand.ts
@@ -1,15 +1,48 @@
-import { neutral, error, brand } from "../global"
+import { neutral, error as _error, brand } from "../global"
+
+const primary = neutral[100]
+const secondary = brand[800]
+const supporting = brand[800]
+const error = _error[500]
+const ctaPrimary = brand[400]
+const ctaSecondary = neutral[100]
+const anchorPrimary = neutral[100]
+
+const root = {
+	primary,
+	secondary,
+	supporting,
+	error,
+	ctaPrimary,
+	ctaSecondary,
+	anchorPrimary,
+}
+
+const button = {
+	buttonPrimary: ctaPrimary,
+	buttonSecondary: ctaSecondary,
+}
+
+const checkbox = {
+	checkbox: primary,
+	checkboxSupporting: supporting,
+	checkboxIndeterminate: supporting,
+}
+
+const link = {
+	linkPrimary: anchorPrimary,
+	linkPrimaryHover: anchorPrimary,
+}
+
+const radio = {
+	radio: primary,
+	radioSupporting: supporting,
+}
+
 export const brandText = {
-	primary: neutral[100],
-	secondary: brand[800],
-	error: error[500],
-	buttonPrimary: brand[400],
-	buttonSecondary: neutral[100],
-	checkbox: neutral[100],
-	checkboxSupporting: brand[800],
-	checkboxIndeterminate: brand[800],
-	linkPrimary: neutral[100],
-	linkPrimaryHover: neutral[100],
-	radio: neutral[100],
-	radioSupporting: brand[800],
+	...root,
+	...button,
+	...checkbox,
+	...link,
+	...radio,
 }
