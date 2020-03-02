92e7979fe491cf02d3daf675642bf41f8159ec47
Simon Adcock <simonadcock2@gmail.com>
2020-03-02 11:46:19 +0000

    move focus style manager to foundations

--- src/core/foundations/rollup.config.js
+++ src/core/foundations/rollup.config.js
@@ -4,19 +4,24 @@ import resolve from "rollup-plugin-node-resolve"
 const extensions = [".ts", ".tsx"]
 const plugins = [babel({ extensions }), resolve({ extensions })]
 
-const folders = ["accessibility", "mq", "palette", "themes", "typography"].map(
-	folder => ({
-		input: `src/${folder}/index.ts`,
-		output: [
-			{
-				file: `${folder}/index.js`,
-				format: "cjs",
-			},
-		],
-		plugins,
-		external: ["@guardian/src-foundations"],
-	}),
-)
+const folders = [
+	"accessibility",
+	"mq",
+	"palette",
+	"themes",
+	"typography",
+	"utils",
+].map(folder => ({
+	input: `src/${folder}/index.ts`,
+	output: [
+		{
+			file: `${folder}/index.js`,
+			format: "cjs",
+		},
+	],
+	plugins,
+	external: ["@guardian/src-foundations"],
+}))
 
 module.exports = [
 	{
