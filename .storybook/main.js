d6c1e23879a7b2c64ae96cc883b8c6f6afb7e6f1
SiAdcock <simonadcock2@gmail.com>
2020-03-11 11:41:35 +0000

    configure storybook viewport plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
 	stories: ["../**/stories.tsx"],
-	addons: ["@storybook/addon-backgrounds"],
+	addons: ["@storybook/addon-backgrounds", "@storybook/addon-viewport"],
 }
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,18 +2,35 @@ import React from "react"
 import { addParameters, addDecorator } from "@storybook/react"
 import { useEffect } from "@storybook/addons"
 import { FocusStyleManager } from "@guardian/src-foundations/utils"
+import { breakpoints } from "@guardian/src-foundations"
 
 const storiesOnly = process.env.NODE_ENV === "production"
-
-// We hide the toolbar by default to make Storybook embeds
-// look nicer in Zeroheight. Related discussion:
-// https://github.com/storybookjs/storybook/issues/8129
-// https://spectrum.chat/zeroheight/feature-requests/swap-out-storybook-stories-in-situ~29d7760d-aba6-4bb3-bf4a-37c119dbc622
+const viewportEntries = Object.entries(breakpoints).map(([name, width]) => {
+	return [
+		name,
+		{
+			name,
+			styles: {
+				width: `${width}px`,
+				height: "100%",
+			},
+		},
+	]
+})
+const viewports = Object.fromEntries(viewportEntries)
 
 addParameters({
+	// We hide the toolbar by default to make Storybook embeds
+	// look nicer in Zeroheight. Related discussion:
+	// https://github.com/storybookjs/storybook/issues/8129
+	// https://spectrum.chat/zeroheight/feature-requests/swap-out-storybook-stories-in-situ~29d7760d-aba6-4bb3-bf4a-37c119dbc622
 	options: {
 		isToolshown: !storiesOnly,
 		isFullscreen: storiesOnly,
+	},
+	viewport: {
+		viewports,
+		defaultViewport: "wide",
 	},
 })
 
