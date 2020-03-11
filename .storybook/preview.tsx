8681f191e769bc8e3b9a81433b6a63f4ed6d2ae0
SiAdcock <simonadcock2@gmail.com>
2020-03-11 11:55:42 +0000

    make viewport dropdown pretty

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -2,18 +2,60 @@ import React from "react"
 import { addParameters, addDecorator } from "@storybook/react"
 import { useEffect } from "@storybook/addons"
 import { FocusStyleManager } from "@guardian/src-foundations/utils"
-import { breakpoints } from "@guardian/src-foundations"
+import { breakpoints, Breakpoint } from "@guardian/src-foundations"
 
 const storiesOnly = process.env.NODE_ENV === "production"
+
+type ViewportMeta = {
+	[key in Breakpoint]: {
+		name: string
+		type: string
+	}
+}
+const viewportMeta: ViewportMeta = {
+	mobile: {
+		name: "Mobile",
+		type: "mobile",
+	},
+	mobileMedium: {
+		name: "Mobile Medium",
+		type: "mobile",
+	},
+	mobileLandscape: {
+		name: "Mobile Landscape",
+		type: "mobile",
+	},
+	phablet: {
+		name: "Phablet",
+		type: "mobile",
+	},
+	tablet: {
+		name: "Tablet",
+		type: "tablet",
+	},
+	desktop: {
+		name: "Desktop",
+		type: "desktop",
+	},
+	leftCol: {
+		name: "Left Col",
+		type: "desktop",
+	},
+	wide: {
+		name: "Wide",
+		type: "desktop",
+	},
+}
 const viewportEntries = Object.entries(breakpoints).map(([name, width]) => {
 	return [
 		name,
 		{
-			name,
+			name: viewportMeta[name].name,
 			styles: {
 				width: `${width}px`,
 				height: "100%",
 			},
+			type: viewportMeta[name].type,
 		},
 	]
 })
