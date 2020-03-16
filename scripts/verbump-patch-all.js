f2fc86cf420680ebe452bd391f20727698bc91b4
Simon Adcock <simonadcock2@gmail.com>
2020-03-16 12:10:05 +0000

    create patch and minor versions of verbump script

---
+++ scripts/verbump-patch-all.js
@@ -0,0 +1,22 @@
+const execa = require("execa")
+const { paths, getComponentPaths } = require("./paths")
+
+const verbump = dir => {
+	return execa("yarn", ["--cwd", `${dir}`, "run", "verbump:patch"], {
+		stdio: "inherit",
+	})
+}
+
+const { root, foundations, svgs } = paths
+
+const packages = getComponentPaths().then(paths =>
+	paths.concat([foundations, svgs, root]),
+)
+
+packages.then(ps => {
+	ps.forEach(dir => {
+		if (!dir) return
+
+		verbump(dir).catch(err => console.log("Error bumping packages:", err))
+	})
+})
