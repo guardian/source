d1d5090fd879f4b0a70de8e6059e00f9936fec11
Simon Adcock <simonadcock2@gmail.com>
2020-03-27 15:18:50 +0000

    ensure packages are built and published in correct order

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -9,29 +9,34 @@ const build = dir => {
 
 const { foundations, svgs, helpers, components } = paths
 
-// heaviy depended on, build these first
-const highPriorityPackages = [foundations, svgs, helpers]
+// heavily depended on, build these first
+const priority0Packages = [foundations, svgs]
+
+// somewhat depended on
+const priority1Packages = [helpers]
 
 // somewhat depended on
 // TODO: try to refactor!
-const mediumPriorityPackages = [`${components}/inline-error`]
+const priority2Packages = [`${components}/inline-error`]
 
 // not depended on
-const lowPriorityPackages = getComponentPaths().then(paths =>
-	paths.filter(path => !mediumPriorityPackages.includes(path)),
+const priority3Packages = getComponentPaths().then(paths =>
+	paths.filter(path => !priority2Packages.includes(path)),
 )
 
-Promise.all(highPriorityPackages.map(dir => build(dir)))
-	.catch(err => console.log("Error building high priority packages:", err))
-	.then(() => Promise.all(mediumPriorityPackages.map(dir => build(dir))))
-	.catch(err => console.log("Error building medium priority packages:", err))
+Promise.all(priority0Packages.map(dir => build(dir)))
+	.catch(err => console.log("Error building priority 0 packages:", err))
+	.then(() => Promise.all(priority1Packages.map(dir => build(dir))))
+	.catch(err => console.log("Error building priority 1 packages:", err))
+	.then(() => Promise.all(priority2Packages.map(dir => build(dir))))
+	.catch(err => console.log("Error building priority 2 packages:", err))
 	.then(() =>
-		lowPriorityPackages.then(packages => {
+		priority3Packages.then(packages => {
 			packages.forEach(package => {
 				if (!package) return
 
 				build(package).catch(err =>
-					console.log("Error building low priority package:", err),
+					console.log("Error building priority 3 package:", err),
 				)
 			})
 		}),
--- scripts/publish-release.js
+++ scripts/publish-release.js
@@ -12,33 +12,36 @@ const publish = dir => {
 	)
 }
 
-const { foundations, svgs, components } = paths
+const { foundations, svgs, helpers, components } = paths
 
 // heavily depended on, publish these first
-const highPriorityPackages = [foundations, svgs]
+const priority0Packages = [foundations, svgs]
+
+// somewhat depended on
+const priority1Packages = [helpers]
 
 // somewhat depended on
 // TODO: try to refactor!
-const mediumPriorityPackages = [`${components}/inline-error`]
+const priority2Packages = [`${components}/inline-error`]
 
 // not depended on
-const lowPriorityPackages = getComponentPaths().then(paths =>
-	paths.filter(path => !mediumPriorityPackages.includes(path)),
+const priority3Packages = getComponentPaths().then(paths =>
+	paths.filter(path => !priority2Packages.includes(path)),
 )
 
-Promise.all(highPriorityPackages.map(dir => publish(dir)))
-	.catch(err => console.log("Error publishing high priority packages:", err))
-	.then(() => Promise.all(mediumPriorityPackages.map(dir => publish(dir))))
-	.catch(err =>
-		console.log("Error publishing medium priority packages:", err),
-	)
+Promise.all(priority0Packages.map(dir => publish(dir)))
+	.catch(err => console.log("Error publishing priority 0 packages:", err))
+	.then(() => Promise.all(priority1Packages.map(dir => publish(dir))))
+	.catch(err => console.log("Error publishing priority 1 packages:", err))
+	.then(() => Promise.all(priority2Packages.map(dir => publish(dir))))
+	.catch(err => console.log("Error publishing priority 2 packages:", err))
 	.then(() =>
-		lowPriorityPackages.then(packages => {
+		priority3Packages.then(packages => {
 			packages.forEach(package => {
 				if (!package) return
 
 				publish(package).catch(err =>
-					console.log("Error publishing low priority package:", err),
+					console.log("Error publishing priority 3 package:", err),
 				)
 			})
 		}),
