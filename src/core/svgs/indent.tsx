7c8af0a9988c2b1fd77f8dd6fa785d84da34803e
Simon Adcock <simonadcock2@gmail.com>
2020-04-06 11:11:25 +0100

    add indent icon

---
+++ src/core/svgs/indent.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const SvgIndent = () => {
+	return (
+		<svg viewBox="0 0 30 20" xmlns="http://www.w3.org/2000/svg">
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M8.02499 9.49998V5H6V10.45L7.025 11.475H20.7499L18 15.05L18.85 15.95L23.9999 10.825V10.15L18.9 5.025L18 5.9L20.7499 9.49998H8.02499Z"
+			/>
+		</svg>
+	)
+}
