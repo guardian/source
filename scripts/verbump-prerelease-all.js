f261ce621c2fca2da49cb1beebe5cb9207bc7298
Simon Adcock <simonadcock2@gmail.com>
2020-04-09 10:13:37 +0100

    add prerelease scripts

---
+++ scripts/verbump-prerelease-all.js
@@ -0,0 +1,22 @@
+const execa = require("execa")
+const { paths, getComponentPaths } = require("./paths")
+
+const verbump = dir => {
+	return execa("yarn", ["--cwd", `${dir}`, "run", "verbump:prerelease"], {
+		stdio: "inherit",
+	})
+}
+
+const { root, foundations, svgs, helpers } = paths
+
+const packages = getComponentPaths().then(paths =>
+	paths.concat([foundations, svgs, root, helpers]),
+)
+
+packages.then(ps => {
+	ps.forEach(dir => {
+		if (!dir) return
+
+		verbump(dir).catch(err => console.log("Error bumping packages:", err))
+	})
+})
