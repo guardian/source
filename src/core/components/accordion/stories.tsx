c446a4d1182b98f963eb40010e36e8cfe2e97d37
Lucy Monie Hall <lucy.monie.hall@gmail.com>
2020-04-09 13:35:29 +0100

    Add new example to stories to show white theme

--- src/core/components/accordion/stories.tsx
+++ src/core/components/accordion/stories.tsx
@@ -1,13 +1,21 @@
 import React from "react"
 import { Accordion, AccordionRow } from "./index"
+import { space } from "@guardian/src-foundations"
+import { css } from "@emotion/core"
 
 export default {
 	title: "Accordion",
 	component: Accordion,
 }
 
+const spacing = css`
+	> * {
+		margin-bottom: ${space[4]}px;
+	}
+`
+
 const defaultLight = () => (
-	<>
+	<div css={spacing}>
 		<Accordion
 			label="Collecting from multiple newsagents"
 			backgroundColour="grey"
@@ -21,7 +29,17 @@ const defaultLight = () => (
 				<a href="">Find your nearest participating retailer</a>
 			</AccordionRow>
 		</Accordion>
-	</>
+
+		<Accordion label="Delivery from your retailer" backgroundColour="white">
+			<AccordionRow>
+				Simply give your preferred store / retailer the barcode printed
+				on your subscription letter.
+			</AccordionRow>
+			<AccordionRow>
+				<a href="">Find your nearest participating retailer</a>
+			</AccordionRow>
+		</Accordion>
+	</div>
 )
 
 defaultLight.story = {
