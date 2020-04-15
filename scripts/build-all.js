520dba8d3cd1172a9f6cfb9fa8d5c63b86e2a14b
Simon Adcock <simonadcock2@gmail.com>
2020-04-15 15:32:40 +0100

    refactored build script to ensure packages are built in right order

--- scripts/build-all.js
+++ scripts/build-all.js
@@ -9,35 +9,38 @@ const build = dir => {
 
 const { foundations, svgs, helpers, coreComponents } = paths
 
-// heavily depended on, build these first
-const priority0Packages = [foundations, svgs]
+// Build these packages in the specified order
+const prioritisedPackages = [
+	foundations,
+	helpers,
+	svgs,
+	`${coreComponents}/inline-error`,
+]
 
-// somewhat depended on
-const priority1Packages = [helpers]
-
-// somewhat depended on
-// TODO: try to refactor!
-const priority2Packages = [`${coreComponents}/inline-error`]
-
-// not depended on
-const priority3Packages = getComponentPaths().then(paths =>
-	paths.filter(path => !priority2Packages.includes(path)),
+// Build these packages in parallel
+const otherPackages = getComponentPaths().then(paths =>
+	paths.filter(path => !prioritisedPackages.includes(path)),
 )
 
-Promise.all(priority0Packages.map(dir => build(dir)))
-	.catch(err => console.log("Error building priority 0 packages:", err))
-	.then(() => Promise.all(priority1Packages.map(dir => build(dir))))
-	.catch(err => console.log("Error building priority 1 packages:", err))
-	.then(() => Promise.all(priority2Packages.map(dir => build(dir))))
-	.catch(err => console.log("Error building priority 2 packages:", err))
-	.then(() =>
-		priority3Packages.then(packages => {
-			packages.forEach(package => {
-				if (!package) return
-
-				build(package).catch(err =>
-					console.log("Error building priority 3 package:", err),
-				)
-			})
-		}),
+prioritisedPackages
+	.reduce(
+		(prev, curr) =>
+			prev
+				.then(() => build(curr))
+				.catch(err =>
+					Promise.reject("Error building prioritised package:", err),
+				),
+		Promise.resolve(),
 	)
+	.then(() =>
+		otherPackages.then(packages =>
+			Promise.all(packages.map(dir => build(dir))).catch(err =>
+				Promise.reject("Error building other packages:", err),
+			),
+		),
+	)
+	.catch(err => {
+		console.log("***BUILD FAILED***\n", err)
+
+		process.exit(1)
+	})
