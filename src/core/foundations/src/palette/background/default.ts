b785cd11641a07deefb90c9e1534d0bbbd5fb2c0
Simon Adcock <simonadcock2@gmail.com>
2020-04-17 23:28:03 +0100

    add secondary background colour to default theme

--- src/core/foundations/src/palette/background/default.ts
+++ src/core/foundations/src/palette/background/default.ts
@@ -2,6 +2,7 @@ import { neutral, brand } from "../global"
 
 export const background = {
 	primary: neutral[100],
+	secondary: neutral[97],
 	inverse: neutral[10],
 	ctaPrimary: brand[400],
 	ctaPrimaryHover: "#234B8A",
