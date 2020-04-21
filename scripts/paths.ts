853d41d5eebfba7c79b207687db9db6345f8f91e
Simon Adcock <simonadcock2@gmail.com>
2020-04-21 16:18:54 +0100

    convert all scripts to typescript

---
+++ scripts/paths.ts
@@ -0,0 +1,59 @@
+import { join } from "path"
+import { readdir, stat } from "fs"
+import { promisify } from "util"
+
+const readdirP = promisify(readdir)
+const statP = promisify(stat)
+
+const root = join(__dirname, "..")
+const foundations = join(__dirname, "../src/core/foundations")
+const svgs = join(__dirname, "../src/core/svgs")
+const helpers = join(__dirname, "../src/core/helpers")
+const coreComponents = join(__dirname, "../src/core/components")
+const editorialComponents = join(__dirname, "../src/editorial/web/components")
+
+const isDirectory = (path: string) =>
+	statP(path).then(stats => stats.isDirectory())
+
+export const getComponentPaths = () =>
+	Promise.all([
+		readdirP(coreComponents)
+			.then(componentDirs =>
+				Promise.all(
+					componentDirs.map(componentDirName =>
+						isDirectory(
+							`${coreComponents}/${componentDirName}`,
+						).then(isDir => {
+							if (!isDir) return
+
+							return `${coreComponents}/${componentDirName}`
+						}),
+					),
+				),
+			)
+			.then(paths => Promise.resolve(paths.filter(path => !!path))),
+		readdirP(editorialComponents)
+			.then(componentDirs =>
+				Promise.all(
+					componentDirs.map(componentDirName =>
+						isDirectory(
+							`${editorialComponents}/${componentDirName}`,
+						).then(isDir => {
+							if (!isDir) return
+
+							return `${editorialComponents}/${componentDirName}`
+						}),
+					),
+				),
+			)
+			.then(paths => Promise.resolve(paths.filter(path => !!path))),
+	]).then(([corePaths, editorialPaths]) => [...corePaths, ...editorialPaths])
+
+export const paths = {
+	root,
+	foundations,
+	svgs,
+	helpers,
+	coreComponents,
+	editorialComponents,
+}
