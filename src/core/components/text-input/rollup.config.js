054437b46ce8a3aa2f805ccf7612f2c5de5d16ab
Simon Adcock <simonadcock2@gmail.com>
2020-04-21 15:10:49 +0100

    point text input cjs build at cjs foundations

--- src/core/components/text-input/rollup.config.js
+++ src/core/components/text-input/rollup.config.js
@@ -10,6 +10,14 @@ module.exports = {
 		{
 			file: "dist/text-input.js",
 			format: "cjs",
+			paths: {
+				"@guardian/src-foundations/themes":
+					"@guardian/src-foundations/themes/cjs",
+				"@guardian/src-foundations/typography":
+					"@guardian/src-foundations/typography/cjs",
+				"@guardian/src-foundations/accessibility":
+					"@guardian/src-foundations/accessibility/cjs",
+			},
 		},
 		{
 			file: "dist/text-input.esm.js",
