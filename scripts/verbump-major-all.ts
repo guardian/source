9ad7b4c1be2853071e34efa05f2505b2c538b3ea
Simon Adcock <simonadcock2@gmail.com>
2020-05-07 15:42:37 +0100

    add major bump scripts

---
+++ scripts/verbump-major-all.ts
@@ -0,0 +1,22 @@
+import execa from "execa"
+import { paths, getComponentPaths } from "./paths"
+
+const verbump = (dir: string) => {
+	return execa("yarn", ["--cwd", `${dir}`, "run", "verbump:major"], {
+		stdio: "inherit",
+	})
+}
+
+const { root, foundations, svgs, helpers } = paths
+
+const packages = getComponentPaths().then(paths =>
+	paths.concat([foundations, svgs, root, helpers]),
+)
+
+packages.then(ps => {
+	ps.forEach(dir => {
+		if (!dir) return
+
+		verbump(dir).catch(err => console.log("Error bumping packages:", err))
+	})
+})
