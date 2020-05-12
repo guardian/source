1e741db09152701d683983853763f0c4e89a696f
Simon Adcock <simonadcock2@gmail.com>
2020-05-12 16:09:21 +0100

    add dependency bumping script

---
+++ scripts/verbump-src-deps-all.ts
@@ -0,0 +1,28 @@
+import { promises as fs } from "fs"
+import { version } from "../package.json"
+import { paths, getComponentPaths } from "./paths"
+
+const verbump = (dir: string) => {
+	return fs.readFile(`${dir}/package.json`, "utf8").then(contents => {
+		// perfectly safe find and replace that definitely, _definitely_ won't go wrong ever
+		const regex = /"@guardian\/src-(\w+)": ".*"/g
+		const replaceString = `"@guardian\/src-$1": "^${version}"`
+		const bumpedContents = contents.replace(regex, replaceString)
+
+		return fs.writeFile(`${dir}/package.json`, bumpedContents)
+	})
+}
+
+const { root, foundations, svgs, helpers } = paths
+
+const packages = getComponentPaths().then(paths =>
+	paths.concat([foundations, svgs, root, helpers]),
+)
+
+packages.then(ps => {
+	ps.forEach(dir => {
+		if (!dir) return
+
+		verbump(dir).catch(err => console.log("Error bumping packages:", err))
+	})
+})
