16ac77704bd77fbaffc3d6e049df36d8363872cf
Simon Adcock <simonadcock2@gmail.com>
2020-05-20 15:42:45 +0100

    resize indent svg to be 30x30

--- src/core/svgs/indent.tsx
+++ src/core/svgs/indent.tsx
@@ -2,11 +2,11 @@ import React from "react"
 
 export const SvgIndent = () => {
 	return (
-		<svg viewBox="0 0 30 20" xmlns="http://www.w3.org/2000/svg">
+		<svg viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
 			<path
 				fillRule="evenodd"
 				clipRule="evenodd"
-				d="M8.02499 9.49998V5H6V10.45L7.025 11.475H20.7499L18 15.05L18.85 15.95L23.9999 10.825V10.15L18.9 5.025L18 5.9L20.7499 9.49998H8.02499Z"
+				d="M8.02499 14.5V10H6V15.45L7.025 16.475H20.7499L18 20.05L18.85 20.95L23.9999 15.825V15.15L18.9 10.025L18 10.9L20.7499 14.5H8.02499Z"
 			/>
 		</svg>
 	)
