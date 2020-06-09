61ab9c7ee787843f1281961c2a01c2f1a44821b5
Simon Adcock <simonadcock2@gmail.com>
2020-06-09 15:07:32 +0100

    add user feddback component

---
+++ src/core/components/user-feedback/styles.ts
@@ -0,0 +1,39 @@
+import { css } from "@emotion/core"
+import { space } from "@guardian/src-foundations"
+import {
+	userFeedbackDefault,
+	UserFeedbackTheme,
+} from "@guardian/src-foundations/themes"
+import { textSans } from "@guardian/src-foundations/typography"
+import { width, height } from "@guardian/src-foundations/size"
+
+const inlineMessage = css`
+	/* we use flex purely for vertical centering */
+	display: flex;
+	align-items: center;
+	${textSans.medium()};
+	/* TODO: we shouldn't be opinionated about layout inside the component */
+	margin-bottom: ${space[1]}px;
+
+	svg {
+		fill: currentColor;
+		/* we don't want the SVG to change size depending on available space */
+		flex: none;
+		width: ${width.iconMedium}px;
+		height: ${height.iconMedium}px;
+	}
+`
+
+export const inlineError = ({
+	userFeedback,
+}: { userFeedback: UserFeedbackTheme } = userFeedbackDefault) => css`
+	${inlineMessage};
+	color: ${userFeedback.textError};
+`
+
+export const inlineSuccess = ({
+	userFeedback,
+}: { userFeedback: UserFeedbackTheme } = userFeedbackDefault) => css`
+	${inlineMessage};
+	color: ${userFeedback.textSuccess};
+`
