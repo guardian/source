7b26b4fa14ae8c9d669b5f0fdb1bb1d60af7a26c
Simon Adcock <simonadcock2@gmail.com>
2020-06-24 16:06:52 +0100

    icon corrections for inline error

--- src/core/components/inline-error/styles.ts
+++ src/core/components/inline-error/styles.ts
@@ -5,19 +5,23 @@ import {
 	InlineErrorTheme,
 } from "@guardian/src-foundations/themes"
 import { textSans } from "@guardian/src-foundations/typography"
+import { remWidth, remHeight } from "@guardian/src-foundations/size"
 
 export const inlineError = ({
 	inlineError,
 }: { inlineError: InlineErrorTheme } = inlineErrorLight) => css`
 	display: flex;
 	align-items: flex-start;
-	${textSans.medium()};
+	${textSans.medium({ lineHeight: "regular" })};
 	color: ${inlineError.text};
 	margin-bottom: ${space[1]}px;
 
 	svg {
 		fill: currentColor;
-		width: 30px;
-		height: 30px;
+		width: ${remWidth.iconMedium}rem;
+		height: ${remHeight.iconMedium}rem;
+
+		/* a visual kick to vertically align the icon with the top row of text */
+		transform: translateY(-3px);
 	}
 `
