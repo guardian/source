1ca7259cfe70558a5f11cedd42f694ae7bda6d27
Simon Adcock <simonadcock2@gmail.com>
2020-06-25 15:49:22 +0100

    add select component boilerplate

---
+++ src/core/components/select/index.tsx
@@ -0,0 +1,91 @@
+import React, {
+	ReactNode,
+	SelectHTMLAttributes,
+	OptionHTMLAttributes,
+} from "react"
+import { InlineError, InlineSuccess } from "@guardian/src-user-feedback"
+import {
+	select,
+	label,
+	errorInput,
+	optionalLabel,
+	supportingText,
+	successInput,
+} from "./styles"
+import { Props } from "@guardian/src-helpers"
+
+export { selectDefault } from "@guardian/src-foundations/themes"
+
+const SupportingText = ({ children }: { children: ReactNode }) => {
+	return (
+		<div css={theme => supportingText(theme.select && theme)}>
+			{children}
+		</div>
+	)
+}
+
+interface SelectProps extends SelectHTMLAttributes<HTMLSelectElement>, Props {
+	label: string
+	optional: boolean
+	supporting?: string
+	error?: string
+	success?: string
+}
+
+const Select = ({
+	label: labelText,
+	optional,
+	supporting,
+	error,
+	success,
+	cssOverrides,
+	children,
+	...props
+}: SelectProps) => {
+	return (
+		<label>
+			<div css={theme => label(theme.select && theme)}>
+				{labelText}{" "}
+				{optional ? (
+					<span css={theme => optionalLabel(theme.select && theme)}>
+						Optional
+					</span>
+				) : (
+					""
+				)}
+			</div>
+			{supporting ? <SupportingText>{supporting}</SupportingText> : ""}
+			{error && <InlineError>{error}</InlineError>}
+			{!error && success && <InlineSuccess>{success}</InlineSuccess>}
+			<select
+				css={theme => [
+					select(theme.select && theme),
+					error ? errorInput(theme.select && theme) : "",
+					!error && success
+						? successInput(theme.select && theme)
+						: "",
+					cssOverrides,
+				]}
+				aria-required={!optional}
+				{...props}
+			>
+				{children}
+			</select>
+		</label>
+	)
+}
+
+interface OptionProps extends OptionHTMLAttributes<HTMLOptionElement>, Props {}
+
+const Option = ({ cssOverrides, children, ...props }: OptionProps) => {
+	return <option {...props}>{children}</option>
+}
+
+const selectDefaultProps = {
+	disabled: false,
+	optional: false,
+}
+
+Select.defaultProps = { ...selectDefaultProps }
+
+export { Select, Option }
---
+++ src/core/components/select/styles.ts
@@ -0,0 +1,68 @@
+import { css } from "@emotion/core"
+import { size, space } from "@guardian/src-foundations"
+import { textSans } from "@guardian/src-foundations/typography"
+import { focusHalo } from "@guardian/src-foundations/accessibility"
+import { selectDefault, SelectTheme } from "@guardian/src-foundations/themes"
+
+export const errorInput = ({
+	select,
+}: { select: SelectTheme } = selectDefault) => css`
+	border: 4px solid ${select.borderError};
+	color: ${select.textError};
+`
+
+export const successInput = ({
+	select,
+}: { select: SelectTheme } = selectDefault) => css`
+	border: 4px solid ${select.borderSuccess};
+	color: ${select.textSuccess};
+`
+
+export const select = ({
+	select,
+}: { select: SelectTheme } = selectDefault) => css`
+	box-sizing: border-box;
+	height: ${size.medium}px;
+	width: 100%;
+	${textSans.medium({ lineHeight: "regular" })};
+	color: ${select.textUserInput};
+	background-color: ${select.backgroundInput};
+	border: 2px solid ${select.border};
+	padding: 0 ${space[2]}px;
+
+	&:active {
+		border: 2px solid ${select.borderActive};
+	}
+
+	&:focus {
+		${focusHalo};
+	}
+
+	&:invalid {
+		${errorInput({ select })};
+	}
+`
+
+export const label = ({
+	select,
+}: { select: SelectTheme } = selectDefault) => css`
+	${textSans.medium({ fontWeight: "bold", lineHeight: "regular" })};
+	color: ${select.textLabel};
+	margin-bottom: ${space[1]}px;
+`
+
+export const optionalLabel = ({
+	select,
+}: { select: SelectTheme } = selectDefault) => css`
+	${textSans.small({ lineHeight: "regular" })};
+	color: ${select.textLabelOptional};
+	font-style: italic;
+`
+
+export const supportingText = ({
+	select,
+}: { select: SelectTheme } = selectDefault) => css`
+	${textSans.small({ lineHeight: "regular" })};
+	color: ${select.textLabelSupporting};
+	margin-bottom: ${space[1]}px;
+`
