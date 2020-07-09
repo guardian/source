03b26fe76643a9843ea6157c0616c92ebff81a9d
Simon Adcock <simonadcock2@gmail.com>
2020-07-09 16:19:09 +0100

    add house and settings icons to svgs package

---
+++ src/core/svgs/house.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const SvgHouse = () => {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
+			<path
+				fillRule="evenodd"
+				clipRule="evenodd"
+				d="M22 25l1-1V13.375L15.6 6h-1.2L7 13.375V24l1 1h4.5v-6.5h5V25H22z"
+			/>
+		</svg>
+	)
+}
