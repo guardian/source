207e7e10969af2eb69c2c6bbd5b1ed230a41a742
Simon Adcock <simonadcock2@gmail.com>
2020-07-17 15:42:44 +0100

    delete deprecated size api

--- src/core/foundations/src/index.ts
+++ src/core/foundations/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./animation"
 export * from "./breakpoints"
 export * from "./palette"
-export * from "./size__legacy"
 export * from "./space"
 
 // Avoid importing the entire palette directly in your application. Prefer using the named exports
