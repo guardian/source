951b8364a9775a6d7feb2a60b263f46b8621974f
Simon Adcock <simonadcock2@gmail.com>
2020-07-30 15:25:21 +0100

    refactor stories into separate modules

---
+++ src/core/components/select/stories/supporting-text.tsx
@@ -0,0 +1,27 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { from } from "@guardian/src-foundations/mq"
+import { Select, Option } from "../index"
+
+const constrainedWith = css`
+	width: 100%;
+	${from.phablet} {
+		width: 25em;
+	}
+`
+export const supportingTextLight = () => (
+	<div css={constrainedWith}>
+		<Select
+			label="State"
+			supporting="Leave blank if you are not within the US"
+		>
+			<Option value="">Select a state</Option>
+			<Option value="al">Alabama</Option>
+			<Option value="ca">California</Option>
+		</Select>
+	</div>
+)
+
+supportingTextLight.story = {
+	name: "supporting text light",
+}
