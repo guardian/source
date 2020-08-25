15597d72cf8c3de3a98d97a4865c268b20507310
Max Duval <max@mxdvl.com>
2020-08-25 21:02:48 +0100

    use palette

--- src/editorial/web/components/lines/squiggly.ts
+++ src/editorial/web/components/lines/squiggly.ts
@@ -1,10 +1,12 @@
+import { border } from "@guardian/src-foundations/palette"
+
 const squigglySvg = encodeURIComponent(`
 <svg
 	xmlns="http://www.w3.org/2000/svg" width="12" height="15"
 	viewBox="0 0 12 15"
 	xmlns:xlink="http://www.w3.org/1999/xlink"
 >
-	<g stroke-width="1" stroke="#DFDFDF" fill="none">
+	<g stroke-width="1" stroke="${border.secondary}" fill="none">
 		<path id="squiggle" d=" M 0 1 q 1.5 0 3 1.5 t 3 1.5 t 3 -1.5 t 3 -1.5" />
 		<use y="3" xlink:href="#squiggle" />
 		<use y="6" xlink:href="#squiggle" />
