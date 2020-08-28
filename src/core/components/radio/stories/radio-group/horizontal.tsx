e835c311fa7346b92d0f04585a7cb022d8f5cbb3
Simon Adcock <simonadcock2@gmail.com>
2020-08-28 15:29:57 +0100

    add radio group stories

---
+++ src/core/components/radio/stories/radio-group/horizontal.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { RadioGroup, Radio } from "../../index"
+
+const horizontal = () => (
+	<RadioGroup orientation="horizontal" name="yes-or-no">
+		<Radio value="yes" label="Yes" />
+		<Radio value="no" label="No" defaultChecked={true} />
+	</RadioGroup>
+)
+horizontal.story = {
+	name: "orientation horizontal",
+}
+
+export { horizontal }
---
+++ src/core/components/radio/stories/radio-group/legend-supporting-text.tsx
@@ -0,0 +1,50 @@
+import React from "react"
+import { storybookBackgrounds } from "@guardian/src-helpers"
+import { RadioGroup, Radio, radioBrand } from "../../index"
+import { ThemeProvider } from "emotion-theming"
+
+/* eslint-disable react/jsx-key */
+const radios = [
+	<Radio value="red" label="Red" />,
+	<Radio value="green" label="Green" defaultChecked={true} />,
+	<Radio value="blue" label="Blue" />,
+]
+/* eslint-enable react/jsx-key */
+
+export const legendSupportingTextLight = () => (
+	<RadioGroup
+		name="colours"
+		label="Select your preferred colour"
+		supporting="You can always change it later"
+	>
+		{radios.map((radio, index) =>
+			React.cloneElement(radio, { key: index }),
+		)}
+	</RadioGroup>
+)
+legendSupportingTextLight.story = {
+	name: `legend supporting text light`,
+}
+
+export const legendSupportingTextBlue = () => (
+	<ThemeProvider theme={radioBrand}>
+		<RadioGroup
+			name="colours"
+			label="Select your preferred colour"
+			supporting="You can always change it later"
+		>
+			{radios.map((radio, index) =>
+				React.cloneElement(radio, { key: index }),
+			)}
+		</RadioGroup>
+	</ThemeProvider>
+)
+
+legendSupportingTextBlue.story = {
+	name: `legend supporting text blue`,
+	parameters: {
+		backgrounds: [
+			Object.assign({}, { default: true }, storybookBackgrounds.brand),
+		],
+	},
+}
