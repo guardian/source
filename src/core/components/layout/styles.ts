934699b28468bad181b1a089c80d96d671f3895f
Max Duval <max.duval@guardian.co.uk>
2020-09-28 13:57:33 +0100

    transform stackSpace into function

--- src/core/components/layout/styles.ts
+++ src/core/components/layout/styles.ts
@@ -7,52 +7,8 @@ export const stack = css`
 	}
 `
 
-export const stackSpace1 = css`
+export const stackSpace = (space) => css`
 	& > * + * {
-		margin-top: ${space[1]}px;
-	}
-`
-export const stackSpace2 = css`
-	& > * + * {
-		margin-top: ${space[2]}px;
-	}
-`
-export const stackSpace3 = css`
-	& > * + * {
-		margin-top: ${space[3]}px;
-	}
-`
-export const stackSpace4 = css`
-	& > * + * {
-		margin-top: ${space[4]}px;
-	}
-`
-export const stackSpace5 = css`
-	& > * + * {
-		margin-top: ${space[5]}px;
-	}
-`
-
-export const stackSpace6 = css`
-	& > * + * {
-		margin-top: ${space[6]}px;
-	}
-`
-
-export const stackSpace9 = css`
-	& > * + * {
-		margin-top: ${space[9]}px;
-	}
-`
-
-export const stackSpace12 = css`
-	& > * + * {
-		margin-top: ${space[12]}px;
-	}
-`
-
-export const stackSpace24 = css`
-	& > * + * {
-		margin-top: ${space[24]}px;
+		margin-top: ${space[space]}px;
 	}
 `
