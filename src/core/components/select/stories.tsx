79c03275aa100209acabd3e28486802aae1440d8
Simon Adcock <simonadcock2@gmail.com>
2020-10-05 11:31:03 +0100

    expose select hide label story

--- src/core/components/select/stories.tsx
+++ src/core/components/select/stories.tsx
@@ -5,6 +5,7 @@ export default {
 export * from "./stories/controlled"
 export * from "./stories/default"
 export * from "./stories/error"
+export * from "./stories/hide-label"
 export * from "./stories/optional"
 export * from "./stories/success"
 export * from "./stories/supporting-text"
