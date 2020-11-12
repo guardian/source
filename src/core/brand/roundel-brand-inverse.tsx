fb5b24a13e5bfacd8682e0031ba563c71a3c543c
Max Duval <max@mxdvl.com>
2020-11-12 15:07:33 -0500

    optimise SVGs
    - use brand colours
    - reduce precision to 2 decimals
    - use circle where relevant

--- src/core/brand/roundel-brand-inverse.tsx
+++ src/core/brand/roundel-brand-inverse.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { SvgRoundelFilled } from "./roundel-filled"
+import { brand } from "@guardian/src-foundations/palette"
 
 export const SvgRoundelBrandInverse = () => {
-	return <SvgRoundelFilled text="#052962" background="white" />
+	return <SvgRoundelFilled text={brand[400]} background="white" />
 }
