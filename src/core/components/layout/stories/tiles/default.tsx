ed3bcba1ec2c285da101eda1df0c2bc44bbdf6df
Francesca Hammond <francesca.hammond@guardian.co.uk>
2020-11-13 13:48:22 +0000

    Add tiles component (#582)
    * Add tiles component
    
    * Add collapse below tablet
    
    * Make code clearer, remove unused space prop
    
    * fix lint errors
    
    Co-authored-by: Simon Adcock <simonadcock2@gmail.com>

---
+++ src/core/components/layout/stories/tiles/default.tsx
@@ -0,0 +1,61 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { Tiles } from "../../index"
+import { textSans } from "@guardian/src-foundations/typography"
+import { sport } from "@guardian/src-foundations/palette"
+
+const contents = css`
+	${textSans.medium()};
+	text-align: center;
+	background-color: ${sport[600]};
+`
+
+export const columns2 = () => (
+	<Tiles columns={2}>
+		<div css={contents}>Item 1</div>
+		<div css={contents}>Item 2</div>
+	</Tiles>
+)
+
+columns2.story = {
+	name: "2 columns",
+}
+
+export const columns3 = () => (
+	<Tiles columns={3}>
+		<div css={contents}>Item 1</div>
+		<div css={contents}>Item 2</div>
+		<div css={contents}>Item 3</div>
+	</Tiles>
+)
+
+columns3.story = {
+	name: "3 columns",
+}
+
+export const columns4 = () => (
+	<Tiles columns={4}>
+		<div css={contents}>Item 1</div>
+		<div css={contents}>Item 2</div>
+		<div css={contents}>Item 3</div>
+		<div css={contents}>Item 4</div>
+	</Tiles>
+)
+
+columns4.story = {
+	name: "4 columns",
+}
+
+export const columns3rows2 = () => (
+	<Tiles columns={3}>
+		<div css={contents}>Item 1</div>
+		<div css={contents}>Item 2</div>
+		<div css={contents}>Item 3</div>
+		<div css={contents}>Item 4</div>
+		<div css={contents}>Item 5</div>
+	</Tiles>
+)
+
+columns3rows2.story = {
+	name: "3 columns 2 rows",
+}
