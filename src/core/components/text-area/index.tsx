f81343c932eae5eaa8150c8ce940219829bed772
Simon Adcock <simonadcock2@gmail.com>
2020-12-04 12:11:10 +0000

    use label package in text area

--- src/core/components/text-area/index.tsx
+++ src/core/components/text-area/index.tsx
@@ -1,24 +1,10 @@
-import React, { ReactNode, InputHTMLAttributes } from "react"
-import { SerializedStyles, css } from "@emotion/core"
+import React, { InputHTMLAttributes } from "react"
+import { SerializedStyles } from "@emotion/core"
 import { InlineError } from "@guardian/src-user-feedback"
-import {
-	widthFluid,
-	textArea,
-	label,
-	errorInput,
-	optionalLabel,
-	supportingText,
-} from "./styles"
+import { Label } from "@guardian/src-label"
+import { widthFluid, textArea, errorInput } from "./styles"
 import { visuallyHidden as _visuallyHidden } from "@guardian/src-foundations/accessibility"
 import { Props } from "@guardian/src-helpers"
-
-const visuallyHidden = css`
-	${_visuallyHidden}
-`
-
-const SupportingText = ({ children }: { children: ReactNode }) => {
-	return <div css={supportingText}>{children}</div>
-}
 
 interface TextAreaProps
 	extends InputHTMLAttributes<HTMLTextAreaElement>,
@@ -61,12 +47,12 @@ const TextArea = ({
 	}
 
 	return (
-		<label>
-			<div css={[label, hideLabel ? visuallyHidden : ""]}>
-				{labelText}{" "}
-				{optional ? <span css={optionalLabel}>Optional</span> : ""}
-			</div>
-			{supporting ? <SupportingText>{supporting}</SupportingText> : ""}
+		<Label
+			text={labelText}
+			supporting={supporting}
+			optional={optional}
+			hideLabel={hideLabel}
+		>
 			{error && <InlineError>{error}</InlineError>}
 			<textarea
 				css={[
@@ -82,7 +68,7 @@ const TextArea = ({
 				className={getClassName()}
 				{...props}
 			/>
-		</label>
+		</Label>
 	)
 }
 
