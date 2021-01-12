bd30b18d9b24d27b935b988469eddb6f55869553
Simon Adcock <simonadcock2@gmail.com>
2021-01-12 11:41:37 +0000

    fix lint issues

--- src/core/components/checkbox/stories/checkbox/required.tsx
+++ src/core/components/checkbox/stories/checkbox/required.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "emotion-theming"
 import { CheckboxGroup, Checkbox, checkboxDefault } from "../../index"
 
 const required = () => (
-	<form onSubmit={e => e.preventDefault()}>
+	<form onSubmit={(e) => e.preventDefault()}>
 		<ThemeProvider theme={checkboxDefault}>
 			<CheckboxGroup name="emails">
 				<Checkbox
@@ -23,7 +23,7 @@ required.story = {
 }
 
 const optional = () => (
-	<form onSubmit={e => e.preventDefault()}>
+	<form onSubmit={(e) => e.preventDefault()}>
 		<ThemeProvider theme={checkboxDefault}>
 			<CheckboxGroup name="emails">
 				<Checkbox
