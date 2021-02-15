e14f2f07fffc5285139233303c6c4c49fd9fbd36
Alex Sanders <a@snd.rs>
2021-02-15 17:50:59 +0000

    expicit index reference

--- src/core/index.ts
+++ src/core/index.ts
@@ -2,20 +2,22 @@
  * @guardian/source
  */
 
-export * from './foundations/src';
-export * from './brand';
-export * from './components/accordion';
-export * from './components/button';
-export * from './components/checkbox';
-export * from './components/choice-card';
-export * from './components/footer';
-export * from './components/label';
-export * from './components/layout';
-export * from './components/link';
-export * from './components/radio';
-export * from './components/select';
-export * from './components/text-area';
-export * from './components/text-input';
-export * from './components/user-feedback';
-export * from './helpers';
-export * from './icons';
+// we need to be explicit about the index file while these directories are also packages,
+// otherwise the node require algorithm uses the package.json
+export * from './foundations/src/index';
+export * from './brand/index';
+export * from './components/accordion/index';
+export * from './components/button/index';
+export * from './components/checkbox/index';
+export * from './components/choice-card/index';
+export * from './components/footer/index';
+export * from './components/label/index';
+export * from './components/layout/index';
+export * from './components/link/index';
+export * from './components/radio/index';
+export * from './components/select/index';
+export * from './components/text-area/index';
+export * from './components/text-input/index';
+export * from './components/user-feedback/index';
+export * from './helpers/index';
+export * from './icons/index';
