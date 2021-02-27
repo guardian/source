29223fedcca284e26411172a51acf5dac0a6548f
Rhys Mills <rhystmills@gmail.com>
2021-02-27 16:20:23 +0000

    Working span with single number of columns per component

--- src/core/components/layout/components/columns/styles.ts
+++ src/core/components/layout/components/columns/styles.ts
@@ -1,6 +1,10 @@
 import { css } from '@emotion/react';
 import { space } from '@guardian/src-foundations';
+<<<<<<< HEAD
 import { Breakpoint, from, until } from '@guardian/src-foundations/mq';
+=======
+import { from, until, between } from '@guardian/src-foundations/mq';
+>>>>>>> c71ea3ce... Working span with single number of columns per component
 
 export const columns = css`
 	box-sizing: border-box;
@@ -151,7 +155,31 @@ export const column = (width?: number | number[]) => {
 
 	return css`
 		box-sizing: border-box;
+<<<<<<< HEAD
 		flex: ${flex};
 		${widthCSS};
+=======
+		/* If a width is specified, don't allow column to grow. Use the width property */
+		flex: ${width ? '0 0 auto' : 1};
+		/*
+		A set of Columns has n columns and n-1 gutters:
+		|    |g|    |g|    |g|    |
+		This means if we take a simple fraction of the width of the set of Columns,
+		our Column will stop part-way through a gutter:
+		|    |g|    |g|    |g|    |
+		|====50%=====|====50%=====|
+		To calculate width of a Column correctly, we must add an imaginary extra gutter
+		and take a fraction of the whole:
+		|    |g|    |g|    |g|    |g|
+		|=====50%=====||====50%=====|
+		This will create a Column which is x columns and x gutters wide. We really want the
+		Column to be x columns and x-1 gutters, so we must subtract a gutter at the end:
+		|    |g|    |g|    |g|    |g|
+		|====50%====| |====50%====|
+	 */
+		${width
+			? `width: calc((100% + ${space[5]}px) * ${width} - ${space[5]}px);`
+			: ''}
+>>>>>>> c71ea3ce... Working span with single number of columns per component
 	`;
 };
