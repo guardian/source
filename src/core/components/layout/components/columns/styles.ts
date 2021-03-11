eb407a25cfd8ef247c1bb382871dd03cee7e28d6
Rhys Mills <rhystmills@gmail.com>
2021-03-11 14:43:58 +0000

    Removed redundant css

--- src/core/components/layout/components/columns/styles.ts
+++ src/core/components/layout/components/columns/styles.ts
@@ -188,8 +188,6 @@ const calculateSpan = (span: number) => {
 	);
 
 	return css`
-		box-sizing: border-box;
-		flex: ${span ? '0 0 auto' : 1};
 		${columnBreakpointCss}
 	`;
 };
@@ -212,6 +210,5 @@ export const setWidth = (value: number | number[]) => css`
 
 // span is specified
 export const setSpan = (value: number | number[]) => css`
-	flex: 0 0 auto;
 	${generateSpanCSS(value)};
 `;
