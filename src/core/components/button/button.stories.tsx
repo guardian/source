8a08306aa4f9769625f0dc92623b3c026602df90
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-03-19 08:52:34 +0000

    break out button and link button stories

---
+++ src/core/components/button/button.stories.tsx
@@ -0,0 +1,8 @@
+export default {
+	title: 'Button',
+};
+
+export * from './stories/icon-only-buttons';
+export * from './stories/priority-buttons';
+export * from './stories/sizes';
+export * from './stories/text-and-icon-buttons';
--- src/core/components/button/link-button.stories.tsx
+++ src/core/components/button/link-button.stories.tsx
@@ -1,11 +1,8 @@
 export default {
-	title: 'Button',
+	title: 'LinkButton',
 };
-export * from './stories/icon-only-buttons';
+
 export * from './stories/icon-only-link-buttons';
-export * from './stories/priority-buttons';
 export * from './stories/priority-link-buttons';
-export * from './stories/sizes';
-export * from './stories/text-and-icon-buttons';
 export * from './stories/text-and-icon-link-buttons';
 export * from './stories/text-and-icon-link-buttons-with-nudge';
