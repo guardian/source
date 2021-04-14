5cd28abb604511d0100d09e5c0b9e10eb672f563
Jamie B <53781962+JamieB-gu@users.noreply.github.com>
2021-04-14 18:59:38 +0100

    Lint fix

--- src/core/helpers/darkMode.ts
+++ src/core/helpers/darkMode.ts
@@ -6,20 +6,23 @@ import { css } from '@emotion/react';
 // ----- Functions ----- //
 
 const darkModeCss = (supportsDarkMode: boolean) => (
-  styles: TemplateStringsArray,
-  ...placeholders: string[]
+	styles: TemplateStringsArray,
+	...placeholders: string[]
 ): SerializedStyles =>
-  supportsDarkMode
-    ? css`
-        @media (prefers-color-scheme: dark) {
-          ${styles
-            .map(
-              (style, i) => `${style}${placeholders[i] ? placeholders[i] : ""}`
-            )
-            .join("")}
-        }
-      `
-    : css``;
+	supportsDarkMode
+		? css`
+				@media (prefers-color-scheme: dark) {
+					${styles
+						.map(
+							(style, i) =>
+								`${style}${
+									placeholders[i] ? placeholders[i] : ''
+								}`,
+						)
+						.join('')}
+				}
+		  `
+		: css``;
 
 // ----- Exports ----- //
 
