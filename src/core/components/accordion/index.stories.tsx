dd3ffd7b8f1b0bc657301d77cb6f2997ca9c4862
Alex Sanders <alex@sndrs.dev>
2021-07-28 12:26:54 +0100

    rename default story to `Demo`

--- src/core/components/accordion/index.stories.tsx
+++ src/core/components/accordion/index.stories.tsx
@@ -6,25 +6,34 @@ export default {
 	subcomponents: { AccordionRow },
 };
 
-export const Default = (args: AccordionProps) => (
+export const Demo = (args: AccordionProps) => (
 	<Accordion {...args}>
 		<AccordionRow label="Collecting from multiple newsagents">
 			Present your card to a newsagent each time you collect the paper.
 			The newsagent will scan your card and will be reimbursed for each
 			transaction automatically.
 		</AccordionRow>
-		<AccordionRow
-			label="Delivery from your retailer"
-			hideToggleLabel={false}
-		>
+		<AccordionRow label="Delivery from your retailer">
 			Simply give your preferred store / retailer the barcode printed on
 			your subscription letter.
 		</AccordionRow>
 	</Accordion>
 );
 
-Default.args = {
+Demo.args = {
 	hideToggleLabel: false,
 };
 
-Default.story = { name: 'Accordion' };
+export const WithCTALabels = (args: AccordionProps) => (
+	<Accordion {...args} hideToggleLabel={false}>
+		<AccordionRow label="" />
+		<AccordionRow label="" />
+	</Accordion>
+);
+
+export const WithoutCTALabels = (args: AccordionProps) => (
+	<Accordion {...args} hideToggleLabel={true}>
+		<AccordionRow label="" />
+		<AccordionRow label="" />
+	</Accordion>
+);
