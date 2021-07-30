97ab3712d59bab66761aa099e2a2c4bac720a0a8
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-07-30 16:44:16 +0100

    Add TextInput docs to storybook

--- src/core/components/text-input/index.tsx
+++ src/core/components/text-input/index.tsx
@@ -1,4 +1,4 @@
-import React, { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes } from 'react';
 import { SerializedStyles } from '@emotion/react';
 import { InlineError, InlineSuccess } from '@guardian/src-user-feedback';
 import { Label } from '@guardian/src-label';
@@ -29,23 +29,63 @@ const widths: {
 	4: width4,
 };
 
-interface TextInputProps extends InputHTMLAttributes<HTMLInputElement>, Props {
+export interface TextInputProps
+	extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value'>,
+		Props {
 	id?: string;
+	/**
+	 * Appears above the text input
+	 */
 	label: string;
+	/**
+	 * Adds the word "Optional" after the label. Non-optional fields are rendered with the `required` attribute.
+	 */
 	optional?: boolean;
+	/**
+	 * Visually hides the label and the "Optional" text set by the `optional` flag.
+	 */
 	hideLabel?: boolean;
+	/**
+	 * Additional text that appears below the label
+	 */
 	supporting?: string;
+	/**
+	 * Determines the width of a fixed-length field, based on the specified character length of the user input. If unspecified,
+	 * the width will default to 100% of the field's container.
+	 */
 	width?: Width;
+	/**
+	 * Whether error styling should apply to this text input. The string appears as an inline error message.
+	 */
 	error?: string;
+	/**
+	 * Whether success styling should apply to this text input. The string appears as an inline success message.
+	 * This prop should not have a value set at the same time as the error prop. In the event that both are set, errors take precedence.
+	 */
 	success?: string;
-	cssOverrides?: SerializedStyles | SerializedStyles[];
+	/**
+	 * The contents of the text input field. This is necessary when using the controlled approach to form state management.
+	 *
+	 * _Note: if you pass the `value` prop, you MUST also pass an `onChange` handler, or the field will be rendered as read-only_
+	 */
+	value?: string;
 }
 
+/**
+ * [Storybook](https://guardian.github.io/source/?path=/docs/source-src-text-area-text-area--demo) •
+ * [Design System](https://theguardian.design/2a1e5182b/p/097455-text-input-field/b/050445) •
+ * [GitHub](https://github.com/guardian/source/tree/main/src/core/components/text-input) •
+ * [NPM](https://www.npmjs.com/package/@guardian/src-text-input)
+ *
+ * Single line fields that allow users to enter freeform data.
+ *
+ * The following themes are supported: `light`
+ */
 const TextInput = ({
 	id,
 	label: labelText,
-	optional,
-	hideLabel,
+	optional = false,
+	hideLabel = false,
 	supporting,
 	width,
 	error,
