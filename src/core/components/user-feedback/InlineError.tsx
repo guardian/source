845521ab39b003c3044600695ab70c864f4718e1
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-08-02 12:48:39 +0100

    User feedback docs (#889)
    * Add reference to helpers in tsconfig
    
    * Break component into separate files and updates stories
    
    * name story exports correctly rather than overriding
    
    * Add link to theguardian.design

---
+++ src/core/components/user-feedback/InlineError.tsx
@@ -0,0 +1,31 @@
+///<reference types="@guardian/src-foundations/types/themes" />
+import { SvgAlertTriangle } from '@guardian/src-icons';
+import { inlineError } from './styles';
+import { UserFeedbackProps } from './types';
+
+/**
+ * [Storybook](https://guardian.github.io/source/?path=/docs/source-src-user-feedback-inlineerror--demo) •
+ * [Design System](https://theguardian.design/2a1e5182b/p/108ed3-user-feedback/b/3803b4) •
+ * [GitHub](https://github.com/guardian/source/tree/main/src/core/components/user-feedback) •
+ * [NPM](https://www.npmjs.com/package/@guardian/src-user-feedback)
+ *
+ * Used to inform the user when a validation has failed or user error has occurred. These messages indicate what went wrong and how to fix it.
+ *
+ * The following themes are supported: `default`, `brand`
+ */
+export const InlineError = ({
+	children,
+	cssOverrides,
+	...props
+}: UserFeedbackProps) => (
+	<span
+		css={(theme) => [
+			inlineError(theme.userFeedback && theme),
+			cssOverrides,
+		]}
+		{...props}
+	>
+		<SvgAlertTriangle />
+		{children}
+	</span>
+);
