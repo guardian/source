3d3d8348183a460fa1464b634aabe44b7c31d0fb
Alex Sanders <alex@sndrs.dev>
2021-08-09 16:04:23 +0100

    describe `SerializedStyles`
    not picked up yet for some reason

--- src/core/helpers/types.ts
+++ src/core/helpers/types.ts
@@ -4,5 +4,8 @@ export type ThemeName = 'default' | 'brand' | 'brandAlt';
 
 export interface Props {
 	className?: string;
+	/**
+	 * The result of using [emotion `css` function/prop](https://emotion.sh/docs/introduction) (or array of them).
+	 */
 	cssOverrides?: SerializedStyles | SerializedStyles[];
 }
