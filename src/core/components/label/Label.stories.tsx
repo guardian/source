ff18222c0a4887ef9f98402332b757c0d39b5607
Simon Adcock <simonadcock2@gmail.com>
2021-08-09 12:11:18 +0100

    use stubbed storybook types in label and legend stories

--- src/core/components/label/Label.stories.tsx
+++ src/core/components/label/Label.stories.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from '@emotion/react';
 import * as themes from '@guardian/src-foundations/themes';
-import type { Args, Story } from '@storybook/react';
+import type { Args, Story } from '../../../@types/storybook-emotion-10-fixes';
+// These types are the right types, but don't work with Storybook v6 which uses Emotion v10
+// import type { Args, Story } from '@storybook/react';
 import { dedent } from 'ts-dedent';
 import { Label, LabelProps } from './Label';
 
