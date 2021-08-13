554a8990b63a7c0801fb11dba83d24c48ef6cadb
Alex Sanders <alex@sndrs.dev>
2021-08-13 10:45:40 +0100

    pull `hide` component up to top level

--- src/core/components/layout/index.tsx
+++ src/core/components/layout/index.tsx
@@ -1,6 +1,6 @@
 export { Columns, Column } from './components/columns/columns';
 export { Container } from './components/container/container';
-export { Hide } from './components/hide/hide';
+export { Hide } from './Hide/Hide';
 export { Stack } from './components/stack/stack';
 export { Tiles } from './components/tiles/tiles';
 export { Inline } from './components/inline/inline';
