232b3a1158573192ca9a9aad955c085c03f9707c
Alex Sanders <alex@sndrs.dev>
2021-08-13 11:03:55 +0100

    add API docs to `Hide`

--- src/core/components/layout/Hide/Hide.tsx
+++ src/core/components/layout/Hide/Hide.tsx
@@ -5,12 +5,36 @@ import { from as fromMq, until as untilMq } from '@guardian/src-foundations/mq';
 import { Props } from '@guardian/src-helpers';
 
 export interface HideProps extends HTMLAttributes<HTMLDivElement>, Props {
-	from?: Breakpoint;
+	/**
+	 * Hide child content if the viewport is narrower than the specified breakpoint.
+	 */
 	until?: Breakpoint;
+	/**
+	 * Hide child content if the viewport is as wide, or wider than, the specified breakpoint.
+	 */
+	from?: Breakpoint;
+	/**
+	 * **Deprecated**
+	 *
+	 * Use `from` instead.
+	 */
 	above?: Breakpoint;
+	/**
+	 * **Deprecated**
+	 *
+	 * Use `until` instead.
+	 */
 	below?: Breakpoint;
 }
 
+/**
+ * [Storybook](https://guardian.github.io/source/?path=/docs/source-src-layout-hide) •
+ * [Design System](https://theguardian.design/2a1e5182b/p/78cb73-hide) •
+ * [GitHub](https://github.com/guardian/source/tree/main/src/core/components/layout/Hide) •
+ * [NPM](https://www.npmjs.com/package/@guardian/src-layout)
+ *
+ * Hides child content `until` and/or `from` a given breakpoint.
+ */
 export const Hide = ({
 	children,
 	above, // deprecated
