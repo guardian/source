bef7b5a99f9be5ab21e490e606a5d542cad247c2
Alex Sanders <alex@sndrs.dev>
2021-08-17 16:40:30 +0100

    add API docs to `Stack`

--- src/core/components/layout/Stack/Stack.tsx
+++ src/core/components/layout/Stack/Stack.tsx
@@ -5,9 +5,20 @@ import { stack, stackSpace } from './styles';
 export type StackSpace = 1 | 2 | 3 | 4 | 5 | 6 | 9 | 12 | 24;
 
 export interface StackProps extends HTMLAttributes<HTMLDivElement>, Props {
+	/**
+	 * [Units of space](https://www.theguardian.design/2a1e5182b/p/449bd5-space) between inline items (one unit is 4px).
+	 */
 	space?: StackSpace;
 }
 
+/**
+ * [Storybook](https://guardian.github.io/source/?path=/docs/source-src-layout-stack) •
+ * [Design System](https://theguardian.design/2a1e5182b/p/827581-stack) •
+ * [GitHub](https://github.com/guardian/source/tree/main/src/core/components/layout/Stack) •
+ * [NPM](https://www.npmjs.com/package/@guardian/src-layout)
+ *
+ * `Stack` components will be stacked one on top of the other.
+ */
 export const Stack = ({
 	cssOverrides,
 	children,
