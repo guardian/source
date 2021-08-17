8de40b2a441f66f28f2d1e88898e1e029eacdef5
Alex Sanders <alex@sndrs.dev>
2021-08-17 16:50:44 +0100

    export `HideProps`

--- src/core/components/layout/index.tsx
+++ src/core/components/layout/index.tsx
@@ -1,6 +1,6 @@
 export { Columns, Column } from './components/columns/columns';
 export { Container, ContainerProps } from './Container/Container';
-export { Hide } from './Hide/Hide';
+export { Hide, HideProps } from './Hide/Hide';
 export { Stack } from './components/stack/stack';
 export { Tiles } from './components/tiles/tiles';
 export { Inline } from './Inline/Inline';
