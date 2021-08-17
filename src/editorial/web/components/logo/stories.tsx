a4875219f11e4856839614324c6ee8911d220ddb
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-08-17 15:38:03 +0100

    feat: Added `src-ed-logo`

---
+++ src/editorial/web/components/logo/stories.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+import { Meta } from '@storybook/react';
+
+import { Logo } from '.';
+
+export default {
+	title: 'Editorial/Logo',
+	component: Logo,
+} as Meta;
+
+const FakeHeader = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div
+			css={css`
+				background-color: #052962;
+				height: 400px;
+				padding: 20px;
+			`}
+		>
+			{children}
+		</div>
+	);
+};
+
+export const Desktop = () => (
+	<FakeHeader>
+		<Logo logoType="standard" />
+		<br />
+		<Logo logoType="anniversary" />
+		<br />
+		<Logo logoType="bestWebsite" />
+	</FakeHeader>
+);
+Desktop.storyName = 'desktop';
+Desktop.parameters = {
+	viewport: {
+		defaultViewport: 'desktop',
+	},
+};
+
+export const Tablet = () => (
+	<FakeHeader>
+		<Logo logoType="standard" />
+		<br />
+		<Logo logoType="anniversary" />
+		<br />
+		<Logo logoType="bestWebsite" />
+	</FakeHeader>
+);
+Tablet.storyName = 'tablet';
+Tablet.parameters = {
+	viewport: {
+		defaultViewport: 'tablet',
+	},
+};
+
+export const Mobile = () => (
+	<FakeHeader>
+		<Logo logoType="standard" />
+		<br />
+		<Logo logoType="anniversary" />
+		<br />
+		<Logo logoType="bestWebsite" />
+	</FakeHeader>
+);
+Mobile.storyName = 'mobile';
+Mobile.parameters = {
+	viewport: {
+		defaultViewport: 'mobileMedium',
+	},
+};
