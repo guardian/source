5baff8de1d3f699ee4a887967e4df5b2e8ce7a29
Alex Sanders <alex@sndrs.dev>
2021-08-18 12:58:07 +0100

    Add API docs for `Tiles`
    update docs

--- src/core/components/layout/Tiles/Tiles.tsx
+++ src/core/components/layout/Tiles/Tiles.tsx
@@ -44,15 +44,33 @@ const collapseBelowColumnsMap: {
 };
 
 export interface TilesProps extends HTMLAttributes<HTMLDivElement>, Props {
+	/**
+	 * The number of columns you want.
+	 */
 	columns: Columns;
+	/**
+	 * Child components will be stacked in a single column at viewport widths narrower than the
+	 * specified breakpoint (they will always be collapsed into a single column below `mobileLandscape`).
+	 */
 	collapseBelow?: CollapseBreakpoint;
 }
 
+/**
+ * [Storybook](https://guardian.github.io/source/?path=/docs/source-src-layout-tiles) •
+ * [Design System](https://theguardian.design/2a1e5182b/p/00e9f5-tiles) •
+ * [GitHub](https://github.com/guardian/source/tree/main/src/core/components/layout/Tiles) •
+ * [NPM](https://www.npmjs.com/package/@guardian/src-layout)
+ *
+ * Child components will be arranged with equal width and spacing with the specified number of columns,
+ * wrapping onto a new row as necessary.
+ *
+ * Below `mobileLandscape`, child components will be collapsed into a single column.
+ */
 export const Tiles = ({
+	columns,
 	collapseBelow,
 	cssOverrides,
 	children,
-	columns,
 	...props
 }: TilesProps) => {
 	return (
