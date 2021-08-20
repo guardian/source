7188c1b8938ea1351ecc014efd0caccf7798042d
Simon Adcock <simonadcock2@gmail.com>
2021-08-20 16:18:30 +0100

    consume in text input theme

--- src/core/foundations/src/themes/text-input.ts
+++ src/core/foundations/src/themes/text-input.ts
@@ -1,14 +1,21 @@
-import { text, background, border } from '@guardian/src-foundations/palette';
+import { background, border } from '@guardian/src-foundations/palette';
 import { userFeedbackDefault } from './user-feedback';
+import {
+	ColorTextUserInput,
+	ColorTextInputLabel,
+	ColorTextSupporting,
+	ColorTextError,
+	ColorTextSuccess,
+} from '../../dist/tokens/color';
 
 export const textInputDefault = {
 	textInput: {
-		textUserInput: text.userInput,
-		textLabel: text.inputLabel,
-		textLabelOptional: text.supporting,
-		textLabelSupporting: text.supporting,
-		textError: text.error,
-		textSuccess: text.success,
+		textUserInput: ColorTextUserInput,
+		textLabel: ColorTextInputLabel,
+		textLabelOptional: ColorTextSupporting,
+		textLabelSupporting: ColorTextSupporting,
+		textError: ColorTextError,
+		textSuccess: ColorTextSuccess,
 		backgroundInput: background.input,
 		border: border.input,
 		borderActive: border.inputActive,
