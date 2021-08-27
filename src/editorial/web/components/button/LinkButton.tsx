fa8f10b4728fea403ff51e373bce37bc0c7d6d0a
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-08-27 12:04:14 +0100

    Update docs and comments

--- src/editorial/web/components/button/LinkButton.tsx
+++ src/editorial/web/components/button/LinkButton.tsx
@@ -11,9 +11,33 @@ import {
 } from './styles';
 
 export interface LinkButtonProps extends CoreLinkButtonProps {
+	/**
+	 * A format object denoting the style of the button using the enums
+	 * available from [@guardian/libs](https://github.com/guardian/libs/blob/main/src/format.ts).
+	 *
+	 * For example:
+	 *
+	 * ```ts
+	 * {
+	 *   display: Display.Standard,
+	 *   design: Design.Article,
+	 *   theme: Pillar.News,
+	 * }
+	 * ```
+	 */
 	format?: Format;
 }
 
+/**
+ * [Storybook](https://guardian.github.io/source/?path=/docs/editorial-src-ed-button-link-button--playground) •
+ * [Design System](https://theguardian.design/2a1e5182b/p/435225-button) •
+ * [GitHub](https://github.com/guardian/source/tree/main/src/editorial/web/components/button) •
+ * [NPM](https://www.npmjs.com/package/@guardian/src-ed-button)
+ *
+ * This is the editorial version of the core Button coomponent.
+ * This editorial version requires the format prop and uses that to override Button styles based on `format.theme`
+ *
+ */
 export const LinkButton = ({
 	format = defaultFormat,
 	children,
