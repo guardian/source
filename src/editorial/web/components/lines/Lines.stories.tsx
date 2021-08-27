22a3b3008eb208a12ac747aef1ecd4a555b00795
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-08-27 12:49:09 +0100

    Update lines docs to latest recommendations

---
+++ src/editorial/web/components/lines/Lines.stories.tsx
@@ -0,0 +1,72 @@
+import { Lines, LinesProps } from './index';
+import type { Story } from '../../../../@types/storybook-emotion-10-fixes';
+import { asPlayground, asChromaticStory } from '../../../../lib/story-intents';
+
+export default {
+	title: 'Editorial/src-ed-lines/Lines',
+	component: Lines,
+	args: {
+		effect: 'straight',
+		count: 4,
+	},
+};
+
+const Template: Story = (args: LinesProps) => <Lines {...args} />;
+
+// *****************************************************************************
+
+export const Playground = Template.bind({});
+asPlayground(Playground);
+
+// *****************************************************************************
+
+export const DefaultLines = Template.bind({});
+asChromaticStory(DefaultLines);
+
+// *****************************************************************************
+
+export const DottedLines = Template.bind({});
+DottedLines.args = {
+	effect: 'dotted',
+};
+asChromaticStory(DottedLines);
+
+// *****************************************************************************
+
+export const SquigglyLines = Template.bind({});
+SquigglyLines.args = {
+	effect: 'squiggly',
+};
+asChromaticStory(SquigglyLines);
+
+// *****************************************************************************
+
+export const DashedLines = Template.bind({});
+DashedLines.args = {
+	effect: 'dashed',
+};
+asChromaticStory(DashedLines);
+
+// *****************************************************************************
+
+export const SingleLine = Template.bind({});
+SingleLine.args = {
+	count: 1,
+};
+asChromaticStory(SingleLine);
+
+// *****************************************************************************
+
+export const EightLines = Template.bind({});
+EightLines.args = {
+	count: 8,
+};
+asChromaticStory(EightLines);
+
+// *****************************************************************************
+
+export const BlueLines = Template.bind({});
+BlueLines.args = {
+	color: 'blue',
+};
+asChromaticStory(BlueLines);
--- src/editorial/web/components/lines/README.md
+++ src/editorial/web/components/lines/README.md
@@ -16,34 +16,10 @@ $ npm i @guardian/src-ed-lines @guardian/src-foundations
 
 ## Use
 
-```jsx
-import { Lines } from '@guardian/src-ed-lines';
+### API
 
-const Section = () => (
-    <>
-        <Lines count={8} />
-        <Lines effect="squiggly" />
-        <Lines count={4} effect="dotted" colour="blue" />
-    </>
-);
-```
+See [storybook](https://guardian.github.io/source/?path=/docs/editorial-src-ed-lines-lines--storybook)
 
-## `Lines` Props
+### How to use
 
-### `effect`
-
-**`"straight" | "squiggly" | "dotted" | "dashed"`** _= "straight"_
-
-The appearance of the lines.
-
-### `count`
-
-**`1 | 4 | 8`** _= 4_
-
-How many lines appear.
-
-### `color`
-
-**`string`** _= line.primary_
-
-The colour of the lines
+For context and visual guides relating to usage see the [Source Design System website](https://www.theguardian.design).
