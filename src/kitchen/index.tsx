006bf57b814ad4c03ea9747b60fc3e2948107409
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-08-31 10:42:10 +0100

    update kitchen index.tsx

--- src/kitchen/index.tsx
+++ src/kitchen/index.tsx
@@ -1,1 +1,5 @@
-export { Lines } from './components/lines';
+export * from './components/button';
+export * from './components/lines';
+export * from './components/logo';
+export * from './components/quote-icon';
+export * from './components/star-rating';
