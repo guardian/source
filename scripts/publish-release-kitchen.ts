80686d4624a3aef67cf463d3ade08eb9b927a90d
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-08-31 15:32:51 +0100

    add release process for kitchen

---
+++ scripts/publish-release-kitchen.ts
@@ -0,0 +1,28 @@
+import execa from 'execa';
+import { paths } from './paths';
+
+const { kitchen } = paths;
+
+console.log('Publishing @guardian/src-kitchen-react');
+
+import(`${kitchen}/package.json`)
+	.then(({ version }) => {
+		return execa(
+			'yarn',
+			[
+				'--cwd',
+				`${kitchen}`,
+				'run',
+				'publish:public',
+				'--new-version',
+				version,
+			],
+			{
+				stdio: 'inherit',
+			},
+		);
+	})
+	.catch((err) => {
+		console.log('***PUBLISH FAILED***\n', err);
+		process.exit(1);
+	});
