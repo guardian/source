dc9cfeaccae16c1f017da02c372f90e8506c6410
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-09-15 15:39:07 +0100

    Create basic script that outputs JSON file

--- scripts/analysis/README.md
+++ scripts/analysis/README.md
@@ -4,11 +4,10 @@ This script runs analysis on a pre-configured list of repositories to discover t
 
 ## What?
 
-The scripts outputs `json` and `csv` files containing:
+The scripts outputs a `json` file containing:
 
 -   A list of repositories and what components they use
 -   A list of components and what repositories use them
--   A list of repositories, what packages they install and which of those packages are actually used
 
 ## Why?
 
@@ -26,7 +25,6 @@ The steps taken are:
     1. Create the `react-scanner` configuration file (using values from the analysis configuration)
     1. Run [`react-scanner`](https://github.com/moroshko/react-scanner)
     1. Extract the results
-    1. Remove the repository
 1. Collate the results from each repository
 1. Write out the results to a `csv` and `json` file
 
@@ -37,9 +35,52 @@ The steps taken are:
 1. Create a `config.json` file in the `scripts/analysis` directory (ask a maintainer for their copy)
 1. Run `yarn analysis`
 
+### Config
+
+The config file looks like:
+
+```json
+{
+    "repos": [
+        {
+            "repo": "discussion-rendering",
+            "projects": [
+                {
+                    "name": "discussion-rendering",
+                    "paths": "src"
+                }
+            ]
+        },
+        {
+            "repo": "dotcom-rendering",
+            "projects": [
+                {
+                    "name": "common-rendering",
+                    "paths": "common-rendering/src"
+                },
+                {
+                    "name": "dotcom-rendering",
+                    "paths": "dotcom-rendering/src"
+                },
+                {
+                    "name": "apps-rendering",
+                    "paths": "apps-rendering/src"
+                }
+            ]
+        }
+    ]
+}
+```
+
+where `paths` is a `|` split list of directories to analyse for a particular project
+
 ## Improvements
 
+-   [ ] Get all the available components and which ones aren't used anywhere
+-   [ ] Write csvs too
+-   [ ] Get a list of repositories, what packages they install and which of those packages are actually used
 -   [ ] Store the configuration in S3 and fetch it from there at the start of the run
 -   [ ] Add checks to ensure that the configuration contains all Source repositories
 -   [ ] Run the script automagically on a schedule
 -   [ ] Add this information to the Source dashboard
+-   [ ] Install `react-scanner` locally instead of using `npx`
--- scripts/analysis/index.ts
+++ scripts/analysis/index.ts
@@ -1,8 +1,118 @@
-const main = async () => {
-	await new Promise((resolve) => {
-		resolve('success');
+import { repos } from './config.json';
+import { cwd, chdir } from 'process';
+import { execSync } from 'child_process';
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs';
+import rimraf from 'rimraf';
+
+const getReactScannerConfig = ({
+	name,
+	paths,
+}: {
+	name: string;
+	paths: string;
+}) => `
+module.exports = {
+	crawlFrom: '.',
+	globs: ['**/(${paths})/**/!(*.test|*.spec).@(js|ts)?(x)'],
+	includeSubComponents: true,
+	importedFrom: /^(@guardian\\/src-).*/,
+	getComponentName: ({ imported, moduleName }) => {
+		return moduleName + '/' + imported;
+	},
+	processors: [
+		['count-components', { outputTo: '${name}.component-usage.json' }],
+	],
+};
+`;
+
+const getStatsByComponent = (
+	byPackage: Record<string, Record<string, number>>,
+) => {
+	const byComponent: Record<string, Record<string, number>> = {};
+	Object.entries(byPackage).forEach(([pkg, components]) => {
+		Object.entries(components).forEach(([component, number]) => {
+			if (Object.keys(byComponent).includes(component)) {
+				byComponent[component][pkg] = number;
+			} else {
+				byComponent[component] = {
+					[pkg]: number,
+				};
+			}
+		});
 	});
+	return byComponent;
+};
+
+const main = async () => {
 	console.log('Running Source analysis ');
+
+	// Get the current working directory so we can restore that at the end
+	// If it's not the root source directory then fall over as the script won't work
+	const _cwd = cwd();
+	if (!_cwd.endsWith(`/source`)) {
+		console.error('This command must be run from the source directory');
+		process.exit(1);
+	}
+
+	try {
+		chdir('./scripts/analysis');
+
+		// The script cleans up after itself so if there's already a tmp directory it might have something
+		// else in it which we don't want to delete
+		if (existsSync('./tmp')) {
+			throw new Error(
+				`${cwd()}/tmp directory already exists. Please remove first.`,
+			);
+		}
+
+		mkdirSync('./tmp');
+		chdir('./tmp');
+
+		const componentUsage: Record<string, Record<string, number>> = {};
+
+		// For each repository, and each project within that repository
+		// Get all of the components that are used
+		for (const repo of repos) {
+			execSync(`git clone git@github.com:guardian/${repo.repo}.git`);
+			chdir(repo.repo);
+			for (const project of repo.projects) {
+				console.log(`Analysing ${project.name}`);
+				const configFileName = `${project.name}.scan.config`;
+				writeFileSync(configFileName, getReactScannerConfig(project));
+				execSync(`npx react-scanner -c ${configFileName}`);
+				componentUsage[project.name] = JSON.parse(
+					readFileSync(
+						`${project.name}.component-usage.json`,
+						'utf-8',
+					),
+				);
+			}
+			chdir('../');
+		}
+
+		chdir('../');
+
+		console.log(`Deleting ${cwd()}/tmp`);
+		rimraf.sync(`${cwd()}/tmp`);
+		console.log(`Formatting data`);
+
+		// Also get the data split by component
+		const data = {
+			by_project: componentUsage,
+			by_component: getStatsByComponent(componentUsage),
+		};
+
+		if (!existsSync('./results')) {
+			mkdirSync('./results');
+		}
+
+		// Write out one JSON file
+		writeFileSync('results/results.json', JSON.stringify(data, null, 2));
+	} catch (e) {
+		console.error(e);
+	} finally {
+		chdir(_cwd);
+	}
 };
 
 if (require.main === module) {
