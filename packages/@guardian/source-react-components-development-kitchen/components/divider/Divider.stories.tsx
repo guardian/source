6356b4f4bba324575df830a63eb62058e3b4c1dc
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-09-23 13:37:07 +0100

    refactor: Removed story wrapper

--- packages/@guardian/source-react-components-development-kitchen/components/divider/Divider.stories.tsx
+++ packages/@guardian/source-react-components-development-kitchen/components/divider/Divider.stories.tsx
@@ -4,69 +4,63 @@ import { Divider } from './Divider';
 export default {
 	title: 'Kitchen/source-react-components-development-kitchen/Divider',
 	component: Divider,
-	parameters: { layout: 'fullscreen' },
 };
 
-const TenPixelPadding = css`
-	/* Use ten pixels to simulate an article body */
-	padding: 10px;
-`;
-
 export const Defaults = () => (
-	<div css={TenPixelPadding}>
+	<span>
 		<h2>The default divider</h2>
 		<p>Some text</p>
 		<Divider />
 		<p>Some text</p>
-	</div>
+	</span>
 );
 Defaults.story = {
 	name: 'with defaults',
 };
 
 export const Tight = () => (
-	<div css={TenPixelPadding}>
+	<span>
 		<h2>When tight</h2>
 		<p>Some text</p>
 		<Divider spaceAbove="tight" />
 		<p>Some text</p>
-	</div>
+	</span>
 );
 Tight.story = {
 	name: 'with less space above',
 };
 
 export const Full = () => (
-	<div css={TenPixelPadding}>
+	<span>
 		<h2>With size full</h2>
 		<p>Some text</p>
 		<Divider size="full" />
 		<p>Some text</p>
-	</div>
+	</span>
 );
 Full.story = {
 	name: 'with size set to full',
 };
 
 export const FullText = () => (
-	<div css={TenPixelPadding}>
+	<span>
 		<h2>With text and size full</h2>
 		<p>Some text</p>
 		<Divider displayText="I am centered" size="full" />
 		<p>Some text</p>
-	</div>
+	</span>
 );
 FullText.story = {
 	name: 'with text and size full',
 };
 
 export const PartialText = () => (
-	<div css={TenPixelPadding}>
+	<span>
 		<h2>With text and size partial</h2>
 		<p>Some text</p>
 		<Divider displayText="I am centered" size="partial" />
 		<p>Some text</p>
-	</div>
+	</span>
 );
 PartialText.story = {
 	name: 'with text and size partial',
