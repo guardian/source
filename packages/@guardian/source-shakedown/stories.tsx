722a78a6580dcca8e1b40e7c6941c339bf4f5fe4
Alex Sanders <alex@sndrs.dev>
2021-09-24 17:01:33 +0100

    try and fail to have a stroy for eall exports

--- packages/@guardian/source-shakedown/stories.tsx
+++ packages/@guardian/source-shakedown/stories.tsx
@@ -1,13 +1,27 @@
-import { Label, labelBrand } from '@guardian/src-label';
-import { isValidElement } from 'react';
+import * as label from '@guardian/src-label';
+import type * as Label from '@guardian/src-label';
+
+console.log(Label);
 
 export default {
 	title: 'Source Shakedown/source-shakedown',
 };
 
-console.log('***Label:', isValidElement(Label));
-console.log('***LabelBrand:', isValidElement(labelBrand));
-
-const LabelDemo = () => <Label text="Who are you?"></Label>;
+// can't find a way to get the possible props without doing storybook-style file analysis
+// which is hard in a package
+const LabelDemo = () =>
+	Object.entries(label).map(([componentName, Component], i) => {
+		console.log(Component, componentName);
+		return typeof Component === 'function' ? (
+			<div style={{ paddingBottom: '1rem' }}>
+				<code>
+					{`import { ${componentName} } from '@guardian/src-label'`};
+				</code>
+				<Component key={i} text="Lorem ipsum dolor sit amet">
+					<span>child component</span>
+				</Component>
+			</div>
+		) : null;
+	});
 
 export { LabelDemo };
