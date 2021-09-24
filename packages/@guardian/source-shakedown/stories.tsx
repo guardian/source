d6ac5fd3eb6099cc645f2ded8692ec96760384b2
Simon Adcock <simonadcock2@gmail.com>
2021-09-24 15:01:38 +0100

    WIP

--- packages/@guardian/source-shakedown/stories.tsx
+++ packages/@guardian/source-shakedown/stories.tsx
@@ -1,8 +1,12 @@
-import { Label } from '@guardian/src-label';
+import { Label, labelBrand } from '@guardian/src-label';
+import { isValidElement } from 'react';
 
 export default {
 	title: 'Source Shakedown/source-shakedown',
 };
+
+console.log('***Label:', isValidElement(Label));
+console.log('***LabelBrand:', isValidElement(labelBrand));
 
 const LabelDemo = () => <Label text="Who are you?"></Label>;
 
