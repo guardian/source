430549552a87a80650391b3d71e02fb7f79bfe52
Simon Adcock <simonadcock2@gmail.com>
2021-09-24 14:39:31 +0100

    add shakedown package

---
+++ packages/@guardian/source-shakedown/stories.tsx
@@ -0,0 +1,9 @@
+import { Label } from '@guardian/src-label';
+
+export default {
+	title: 'Source Shakedown/source-shakedown',
+};
+
+const LabelDemo = () => <Label text="Who are you?"></Label>;
+
+export { LabelDemo };
