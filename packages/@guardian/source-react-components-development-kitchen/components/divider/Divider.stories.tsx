7681445de105b71a6a7739c122f5e37cd7211fea
Oliver Lloyd <oliverlloyd@users.noreply.github.com>
2021-09-27 12:13:55 +0100

    chore: Use better example text in stories

--- packages/@guardian/source-react-components-development-kitchen/components/divider/Divider.stories.tsx
+++ packages/@guardian/source-react-components-development-kitchen/components/divider/Divider.stories.tsx
@@ -12,9 +12,22 @@ export default {
 
 const Template: Story = (args: DividerProps) => (
 	<span>
-		<p>Some text</p>
+		<p>
+			The individual responsible for one of the most significant leaks in
+			US political history is Edward Snowden, a 29-year-old former
+			technical assistant for the CIA and current employee of the defence
+			contractor Booz Allen Hamilton. Snowden has been working at the
+			National Security Agency for the last four years as an employee of
+			various outside contractors, including Booz Allen and Dell.
+		</p>
 		<Divider {...args} />
-		<p>Some text</p>
+		<p>
+			The Guardian, after several days of interviews, is revealing his
+			identity at his request. From the moment he decided to disclose
+			numerous top-secret documents to the public, he was determined not
+			to opt for the protection of anonymity. “I have no intention of
+			hiding who I am because I know I have done nothing wrong,” he said.
+		</p>
 	</span>
 );
 
