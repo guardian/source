4e81c3397a14c467bd5162e0e5419d9dfdf2b874
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-09-29 09:05:17 +0100

    Add a script to analyse Source usage (#972)
    * add initial analysis script
    
    * Create basic script that outputs JSON file
    
    * fix README typo
    
    * add more info to README
    
    * Get unused components
    
    * Use local react-scanner
    
    * Use shallow clone to make it faster
    
    * write csv file
    
    * ignore foundations package
    
    * Fix typo in export name
    
    Co-authored-by: Simon Adcock <simonadcock2@gmail.com>
    
    * update analysis for development kitchen
    
    * implement review suggestions
    
    * rename everything to get-usage for consistency
    
    * Add config file
    
    * Apply suggestions from code review
    
    Co-authored-by: Simon Adcock <simonadcock2@gmail.com>
    
    Co-authored-by: Simon Adcock <simonadcock2@gmail.com>

---
+++ scripts/get-usage/get-all-components.ts
@@ -0,0 +1,91 @@
+import { readdirSync, readFileSync } from 'fs';
+import {
+	packageNames,
+	getComponentPackageNamesWithPaths,
+} from '../package-names';
+import { paths, getKitchenComponentPaths } from '../paths';
+import { parse } from '@typescript-eslint/typescript-estree';
+
+const getPackageComponents = (path: string | string[]): string[] => {
+	if (Array.isArray(path)) {
+		return path.flatMap((p) => getPackageComponents(p));
+	}
+
+	const contents = readdirSync(path);
+
+	if (contents.includes('index.tsx')) {
+		return getExportsFromFile(`${path}/index.tsx`);
+	} else {
+		return contents
+			.filter(
+				(item) =>
+					!item.endsWith('stories.tsx') && item.endsWith('.tsx'),
+			)
+			.flatMap((file) => getExportsFromFile(`${path}/${file}`));
+	}
+};
+
+const getExportsFromFile = (path: string): string[] => {
+	const contents = readFileSync(path, 'utf8');
+
+	const ast = parse(contents, { jsx: true });
+	const exports: string[] = [];
+
+	for (const node of ast.body) {
+		if (
+			node.type !== 'ExportNamedDeclaration' ||
+			node.exportKind !== 'value'
+		)
+			continue;
+
+		if (node.declaration?.type === 'VariableDeclaration') {
+			for (const declaration of node.declaration.declarations) {
+				if (declaration.id.type === 'Identifier') {
+					const firstChar = declaration.id.name[0];
+					if (
+						firstChar === firstChar.toUpperCase() &&
+						!declaration.id.name.endsWith('Props')
+					) {
+						exports.push(declaration.id.name);
+					}
+				}
+			}
+		}
+
+		for (const specifier of node.specifiers) {
+			const firstChar = specifier.exported.name[0];
+			if (
+				firstChar === firstChar.toUpperCase() &&
+				!specifier.exported.name.endsWith('Props')
+			) {
+				exports.push(specifier.exported.name);
+			}
+		}
+	}
+
+	return exports;
+};
+
+export const getAllComponentsAndPackages = async () => {
+	const packages: Record<string, string | string[]> = {
+		[packageNames.brand]: paths.brand,
+		[packageNames.icons]: paths.icons,
+		[packageNames.kitchen]: await getKitchenComponentPaths(),
+		...(await getComponentPackageNamesWithPaths()),
+	};
+
+	const components: Record<string, string[]> = {};
+
+	Object.entries(packages).forEach(([pkg, path]) => {
+		components[pkg] = getPackageComponents(path);
+	});
+
+	return {
+		componetsByPackage: components,
+		components: Object.values(components).flatMap((c) => c),
+		packages: Object.keys(components),
+		componentsWithPackage: Object.entries(components).flatMap(([pkg, cs]) =>
+			cs.flatMap((c) => `${pkg}/${c}`),
+		),
+	};
+};
