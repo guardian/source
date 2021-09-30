783a166f5d65f34d14e85e79a7689401e085891b
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-09-30 15:05:05 +0100

    Export all component prop types

--- src/core/components/checkbox/index.tsx
+++ src/core/components/checkbox/index.tsx
@@ -4,4 +4,6 @@ export {
 } from '@guardian/src-foundations/themes';
 
 export { CheckboxGroup } from './CheckboxGroup';
+export type { CheckboxGroupProps } from './CheckboxGroup';
 export { Checkbox } from './Checkbox';
+export type { CheckboxProps } from './Checkbox';
