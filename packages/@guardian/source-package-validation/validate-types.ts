5122ba3e3f135cdc62063c08e77c8e5de1ac630b
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-10-05 12:09:10 +0100

    use export to simplify type checker

--- packages/@guardian/source-package-validation/validate-types.ts
+++ packages/@guardian/source-package-validation/validate-types.ts
@@ -1,4 +1,4 @@
-import type {
+export type {
 	Breakpoint,
 	ScaleUnit,
 	Category,
@@ -19,7 +19,7 @@ import type {
 	FontScaleArgs,
 } from '@guardian/source-foundations';
 
-import type {
+export type {
 	EditorialButtonProps,
 	EditorialLinkButtonProps,
 	LineCount,
@@ -30,7 +30,7 @@ import type {
 	StarRatingProps,
 } from '@guardian/source-react-components-development-kitchen';
 
-import type {
+export type {
 	SvgGuardianLiveLogoProps,
 	SvgGuardianLogoProps,
 	SvgRoundelBrandProps,
@@ -39,12 +39,12 @@ import type {
 	SvgRoundelInverseProps,
 } from '@guardian/src-brand';
 
-import type {
+export type {
 	AccordionProps,
 	AccordionRowProps,
 } from '@guardian/src-accordion';
 
-import type {
+export type {
 	ButtonPriority,
 	IconSide,
 	Size,
@@ -52,19 +52,19 @@ import type {
 	LinkButtonProps,
 } from '@guardian/src-button';
 
-import type { CheckboxGroupProps, CheckboxProps } from '@guardian/src-checkbox';
+export type { CheckboxGroupProps, CheckboxProps } from '@guardian/src-checkbox';
 
-import type {
+export type {
 	ChoiceCardGroupProps,
 	ChoiceCardColumns,
 	ChoiceCardProps,
 } from '@guardian/src-choice-card';
 
-import type { FooterProps } from '@guardian/src-footer';
+export type { FooterProps } from '@guardian/src-footer';
 
-import type { LabelProps, LegendProps } from '@guardian/src-label';
+export type { LabelProps, LegendProps } from '@guardian/src-label';
 
-import type {
+export type {
 	ColumnsProps,
 	ColumnProps,
 	ContainerProps,
@@ -74,110 +74,24 @@ import type {
 	InlineProps,
 } from '@guardian/src-layout';
 
-import type {
+export type {
 	LinkPriority,
 	LinkProps,
 	ButtonLinkProps,
 } from '@guardian/src-link';
 
-import type { RadioGroupProps, RadioProps } from '@guardian/src-radio';
+export type { RadioGroupProps, RadioProps } from '@guardian/src-radio';
 
-import type { OptionProps, SelectProps } from '@guardian/src-select';
+export type { OptionProps, SelectProps } from '@guardian/src-select';
 
-import type { TextAreaProps } from '@guardian/src-text-area';
+export type { TextAreaProps } from '@guardian/src-text-area';
 
-import type { TextInputProps } from '@guardian/src-text-input';
+export type { TextInputProps } from '@guardian/src-text-input';
 
-import type { UserFeedbackProps } from '@guardian/src-user-feedback';
+export type { UserFeedbackProps } from '@guardian/src-user-feedback';
 
-import type { ThemeName, Props } from '@guardian/src-helpers';
+export type { ThemeName, Props } from '@guardian/src-helpers';
 
-import type { Breakpoint as SrcBreakpoint } from '@guardian/src-foundations';
+export type { Breakpoint as SrcBreakpoint } from '@guardian/src-foundations';
 
-import type { IconSize, IconProps } from '@guardian/src-icons';
-
-// The types have to be used somewhere or tsc falls over
-export type SourceFoundations =
-	| Breakpoint
-	| ScaleUnit
-	| Category
-	| LineHeight
-	| FontWeight
-	| FontStyle
-	| FontWeightDefinition
-	| Option<Breakpoint>
-	| TypographyStyles
-	| TypographySizes
-	| TitlepieceSizes
-	| HeadlineSizes
-	| BodySizes
-	| TextSansSizes
-	| Fs
-	| FontScaleFunction
-	| FontScaleFunctionStr
-	| FontScaleArgs;
-
-export type SourceReactComponentsDevelopmentKitchen =
-	| EditorialButtonProps
-	| EditorialLinkButtonProps
-	| LineCount
-	| LinesProps
-	| LogoProps
-	| HeadlineSize
-	| QuoteIconProps
-	| StarRatingProps;
-
-export type SrcBrand =
-	| SvgGuardianLiveLogoProps
-	| SvgGuardianLogoProps
-	| SvgRoundelBrandProps
-	| SvgRoundelBrandInverseProps
-	| SvgRoundelDefaultProps
-	| SvgRoundelInverseProps;
-
-export type SrcAccordion = AccordionProps | AccordionRowProps;
-
-export type SrcButton =
-	| ButtonPriority
-	| IconSide
-	| Size
-	| ButtonProps
-	| LinkButtonProps;
-
-export type SrcCheckbox = CheckboxGroupProps | CheckboxProps;
-
-export type SrcChoiceCard =
-	| ChoiceCardGroupProps
-	| ChoiceCardColumns
-	| ChoiceCardProps;
-
-export type SrcFooter = FooterProps;
-
-export type SrcLabel = LabelProps | LegendProps;
-
-export type SrcLayout =
-	| ColumnsProps
-	| ColumnProps
-	| ContainerProps
-	| HideProps
-	| StackProps
-	| TilesProps
-	| InlineProps;
-
-export type SrcLink = LinkPriority | LinkProps | ButtonLinkProps;
-
-export type SrcRadio = RadioGroupProps | RadioProps;
-
-export type SrcSelect = OptionProps | SelectProps;
-
-export type SrcTextArea = TextAreaProps;
-
-export type SrcTextInput = TextInputProps;
-
-export type SrcUserFeedback = UserFeedbackProps;
-
-export type SrcHelpers = ThemeName | Props;
-
-export type SrcFoundations = SrcBreakpoint;
-
-export type SrcIcons = IconSize | IconProps;
+export type { IconSize, IconProps } from '@guardian/src-icons';
