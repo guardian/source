0529d31df6dbf4716578bad4aa4916b17acb7da4
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-10-05 10:35:07 +0100

    ignore package validation tests in root jest config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	testPathIgnorePatterns: ['/node_modules', '/dist'],
+	testPathIgnorePatterns: [
+		'/node_modules',
+		'/dist',
+		'packages/@guardian/source-package-validation',
+	],
 };
