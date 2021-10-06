d45c88662b93123e2795b1df3ac5a1a34fc3c053
Alex Sanders <alex@sndrs.dev>
2021-10-06 17:04:03 +0100

    create `source-react-components`
    and export `Label` and `Legend`

---
+++ packages/@guardian/source-react-components/src/index.ts
@@ -0,0 +1,3 @@
+// export { labelBrand, labelDefault } from '@guardian/src-foundations/themes';
+export { Label, Legend } from '@guardian/src-label';
+export type { LabelProps, LegendProps } from '@guardian/src-label';
