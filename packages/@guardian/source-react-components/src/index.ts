c849862e64ea530a60a137168b2469ff8bbf0013
Alex Sanders <alex@sndrs.dev>
2021-10-06 17:15:36 +0100

    move accordion to packages

--- packages/@guardian/source-react-components/src/index.ts
+++ packages/@guardian/source-react-components/src/index.ts
@@ -1,3 +1,10 @@
-// export { labelBrand, labelDefault } from '@guardian/src-foundations/themes';
+export { Accordion, AccordionRow } from '@guardian/src-accordion';
+export type {
+	AccordionProps,
+	AccordionRowProps,
+} from '@guardian/src-accordion';
+// export { accordionDefault } from '@guardian/src-foundations/themes';
+
 export { Label, Legend } from '@guardian/src-label';
 export type { LabelProps, LegendProps } from '@guardian/src-label';
+// export { labelBrand, labelDefault } from '@guardian/src-foundations/themes';
