de5ab002b7af06b0e4a291c7e41e2236584e8f62
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-10-28 13:39:06 +0100

    add interval to check and click docs button

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -22,3 +22,29 @@ export const parameters = {
 };
 
 export const decorators = [FocusManagerDecorator, ThemeProviderDecorator];
+
+// Hacky way of making sure that we're on the docs page if it's available
+// Polls for href changes as there's not event emitted when changing stories
+// Inspired by https://github.com/storybookjs/storybook/issues/13128#issuecomment-798927176
+var currentPage = window.location.href;
+window.setInterval(() => {
+	if (window.location.href !== currentPage) {
+		currentPage = window.location.href;
+		try {
+			// Find all buttons containing the text Docs within the <div role="main"> tag
+			const docsButtonSelector = window.parent.document.evaluate(
+				"//div[@role='main']//button[contains(., 'Docs')]",
+				window.parent.document,
+				null,
+				XPathResult.ANY_TYPE,
+				null,
+			);
+
+			const button =
+				docsButtonSelector.iterateNext() as HTMLButtonElement;
+			button && button.click();
+		} catch (error) {
+			console.error(error);
+		}
+	}
+}, 500);
