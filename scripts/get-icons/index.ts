2adb76adc4f5cbe2f9bb8c96872655571eafc223
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-11-01 15:59:59 +0000

    write svgs to disk

--- scripts/get-icons/index.ts
+++ scripts/get-icons/index.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
-// import fs from 'fs';
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
 
 const ICON_FILE = 'Ai7AELHC6KCz38qKZkvuHo';
 
 const ICON_FRAME = 'UI icons 24(w)x24(w)';
+
+const OUTPUT_DIR = './svgs';
 
 const FIGMA_OPTIONS = {
 	headers: {
@@ -23,6 +25,54 @@ interface FigmaComponentsResponse {
 	};
 }
 
+interface Node {
+	name: string;
+	node_id: string;
+}
+
+interface NodeWithUrl extends Node {
+	url: string;
+}
+
+const getUrlsForNodes = (nodes: Node[]): Promise<NodeWithUrl[]> => {
+	return new Promise((resolve) => {
+		return axios
+			.get(
+				`https://api.figma.com/v1/images/${ICON_FILE}/?ids=${nodes
+					.map(({ node_id }) => node_id)
+					.join(',')}&format=svg`,
+				FIGMA_OPTIONS,
+			)
+			.then((res) => {
+				return resolve(
+					nodes.map((node) => ({
+						...node,
+						url: res.data.images[node.node_id],
+					})),
+				);
+			});
+	});
+};
+
+const getAndWriteSVGForNode = (node: NodeWithUrl) => {
+	return new Promise((resolve) => {
+		return axios
+			.get(node.url)
+			.then((res) => {
+				return resolve(
+					writeFileSync(`svgs/${node.name}.svg`, res.data),
+				);
+			})
+			.catch((err) => {
+				console.log(`Failed to write SVG for ${node.name}: ${err}`);
+			});
+	});
+};
+
+if (!existsSync(OUTPUT_DIR)) {
+	mkdirSync(OUTPUT_DIR);
+}
+
 axios
 	.get<FigmaComponentsResponse>(
 		`https://api.figma.com/v1/files/${ICON_FILE}/components`,
@@ -39,27 +89,15 @@ axios
 					name,
 				};
 			});
-
-		return axios.get(
-			`https://api.figma.com/v1/images/${ICON_FILE}/?ids=${svgNodes
-				.map(({ node_id }) => node_id)
-				.join(',')}&format=svg`,
-			FIGMA_OPTIONS,
-		);
+		return getUrlsForNodes(svgNodes);
 	})
-	.then((res) => {
-		const images = Object.values(res.data.images) as string[];
-
+	.then((nodes) => {
 		return Promise.all(
-			images.map((imageUrl) => {
-				return axios.get(imageUrl);
+			nodes.map((node) => {
+				return getAndWriteSVGForNode(node);
 			}),
 		);
 	})
-	.then((res) => {
-		console.log(res[0].data);
-
-		// fs.writeFileSync('image.svg', res[0].data, {
-		// 	encoding: 'utf-8',
-		// });
+	.then(() => {
+		console.log('Icons imported successfully');
 	});
