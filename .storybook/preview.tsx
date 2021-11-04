d465a0d7b4bc4fcc77c8a3cb7cd7c6df238fc5c7
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-11-04 10:23:25 +0000

    Ensure hash links work on first load

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -51,3 +51,16 @@ window.setInterval(() => {
 		}
 	}
 }, 500);
+
+// The docs pages don't seem to respect the anchor links on load so this
+// ensures they do. This may be related to the fact that window.location.hash
+// isn't set on first page load, hence we're getting it from window.parent.location.hash
+// Something else is also going on to affect scroll, hence the need for the timeout (sorry!)
+window.addEventListener('load', () => {
+	if (window.location.hash !== window.parent.location.hash) {
+		setTimeout(
+			() => (window.location.hash = window.parent.location.hash),
+			10,
+		);
+	}
+});
