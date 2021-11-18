b55aca3dce1c9dd4542f26404798821e5b23c6e6
Jamie Lynch <jamie.lynch@guardian.co.uk>
2021-11-18 16:16:16 +0000

    Include new line at end of file

--- scripts/get-icons/format.ts
+++ scripts/get-icons/format.ts
@@ -3,6 +3,7 @@ import { html, HTMLBeautifyOptions } from 'js-beautify';
 const options: HTMLBeautifyOptions = {
 	wrap_attributes: 'force-expand-multiline',
 	indent_with_tabs: true,
+	end_with_newline: true,
 };
 
 export const formatSVG = (svg: string): string => {
