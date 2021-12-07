90fac406608adb52d64f9b8475dfb6ace0df1a2f
Alex Sanders <alex@sndrs.dev>
2021-12-07 19:00:50 +0000

    remove `story-sync`
    no longer needed

--- scripts/story-sync/sync.ts
+++
@@ -1,8 +0,0 @@
-import { getV3Stories } from './get-stories';
-import { writeStories } from './write-stories';
-
-console.log('Syncing v3 and v4 stories');
-
-const v3Stories = getV3Stories();
-
-writeStories(v3Stories);
