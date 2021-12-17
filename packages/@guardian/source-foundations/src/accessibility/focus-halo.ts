9ebcf00e4a35ea7a986654d099288e34b81f25f5
Alex Sanders <alex@sndrs.dev>
2021-12-17 11:34:40 +0000

    half finished ideas

--- packages/@guardian/source-foundations/src/accessibility/focus-halo.ts
+++ packages/@guardian/source-foundations/src/accessibility/focus-halo.ts
@@ -3,7 +3,7 @@ import { border } from '../colour/palette';
 /**
  * [Storybook](https://guardian.github.io/source/?path=/docs/packages-source-foundations-accessibility-helpers--page#focus-halo)
  *
- * Provides a clear focus state to support keyboard navigation.
+ * CSS rules that provide a clear visual focus state to support keyboard navigation.
  */
 export const focusHalo = `
  outline: 0;
