1beef5081d6a01aeaf2c50d179b5ba3178be1858
Max Duval <max.duval@theguardian.com>
2022-02-15 16:16:10 +0000

    add circle arrow clockwise
    sized 24×24, not the usual 30×30

---
+++ packages/@guardian/source-react-components/src/icons/SvgArrowCircleClockwise.tsx
@@ -0,0 +1,15 @@
+import type { EmotionJSX } from '@emotion/react/types/jsx-namespace';
+import { iconSize } from '@guardian/source-foundations';
+import type { IconProps } from './types';
+
+export const SvgArrowCircleClockwise = ({
+	size,
+}: IconProps): EmotionJSX.Element => (
+	<svg
+		viewBox="0 0 24 24"
+		width={size ? iconSize[size] : '24'}
+		height={size ? iconSize[size] : '24'}
+	>
+		<path d="M11.588 1a10.928 10.928 0 0 0-9.046 4.786l.126.676.877.527.676-.176C5.85 4.508 8.506 2.98 11.588 2.98c4.936 0 8.995 4.059 8.995 9.045 0 4.961-4.059 8.995-8.995 8.995-2.832 0-5.262-1.252-6.94-3.257l3.632-.601v-1.253H1.866l-.476.476V23h1.227l.627-3.784C5.248 21.546 8.205 23 11.588 23c6.089 0 11.025-4.911 11.025-10.975A11.01 11.01 0 0 0 11.588 1Z" />
+	</svg>
+);
