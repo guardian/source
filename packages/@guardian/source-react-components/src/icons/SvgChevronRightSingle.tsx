d9ee1caa5dac0982a524d9f86711650e5f5fd878
tjsilver <thalia.silver@guardian.co.uk>
2022-03-07 10:19:37 +0000

    add a11y label to chevronRightSingle icon

--- packages/@guardian/source-react-components/src/icons/SvgChevronRightSingle.tsx
+++ packages/@guardian/source-react-components/src/icons/SvgChevronRightSingle.tsx
@@ -1,15 +1,19 @@
+import { css } from '@emotion/react';
 import type { EmotionJSX } from '@emotion/react/types/jsx-namespace';
-import { iconSize } from '@guardian/source-foundations';
+import { iconSize, visuallyHidden } from '@guardian/source-foundations';
 import type { IconProps } from './types';
 
 export const SvgChevronRightSingle = ({
 	size,
-}: IconProps): EmotionJSX.Element => {
-	return (
+	isAnnouncedByScreenReader = false,
+}: IconProps): EmotionJSX.Element => (
+	<>
 		<svg
 			viewBox="0 0 30 30"
 			xmlns="http://www.w3.org/2000/svg"
 			width={size ? iconSize[size] : undefined}
+			aria-hidden={true}
+			focusable={false}
 		>
 			<path
 				fillRule="evenodd"
@@ -17,5 +21,16 @@ export const SvgChevronRightSingle = ({
 				d="M9.975 4L9 4.95L17.325 14.925L9 24.8999L9.975 25.8499L20.375 15.45V14.4L9.975 4Z"
 			/>
 		</svg>
-	);
-};
+		{isAnnouncedByScreenReader ? (
+			<span
+				css={css`
+					${visuallyHidden}
+				`}
+			>
+				Chevron right
+			</span>
+		) : (
+			''
+		)}
+	</>
+);
