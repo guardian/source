04a91b5fdbd500e6bfe588ea1e7a1c8ae364ec37
Alex Sanders <alex@sndrs.dev>
2022-03-18 15:43:48 +0000

    deprecate the public `Props` export

--- packages/@guardian/source-react-components/src/index.ts
+++ packages/@guardian/source-react-components/src/index.ts
@@ -186,4 +186,8 @@ export {
 	userFeedbackThemeDefault,
 } from './user-feedback/theme';
 
-export type { Props } from './@types/Props';
+/**
+ * @deprecated This is for internal use only. It was previously exported so the v3 `src-*` modules could use it.
+ * It will be removed from the public interface in v5.
+ */
+export type Props = import('./@types/Props').Props; // eslint-disable-line @typescript-eslint/consistent-type-imports -- we want to apply the jsdoc @deprecated tag only the the public export, not the actual type itself
