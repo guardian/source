cf4e9e80c839b20cd4d8ad8608358ba77784ef16
Alex Sanders <alex@sndrs.dev>
2022-04-29 13:14:13 +0100

    remove old `get-icons` scripts

--- scripts/get-icons/case.ts
+++
@@ -1,6 +0,0 @@
-export const kebabToTitle = (name: string): string => {
-	return name
-		.split('-')
-		.map((s) => s.substr(0, 1).toLocaleUpperCase() + s.substring(1))
-		.join('');
-};
--- scripts/get-icons/log.ts
+++
@@ -1,5 +0,0 @@
-export class Log {
-	static error = (message: string) => {
-		console.error(`\x1b[31m${message}\x1b[0m`);
-	};
-}
