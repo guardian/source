8b5726bbc527155b67262e0fd31454b621f06553
tjsilver <thalia.silver@guardian.co.uk>
2022-05-23 16:28:57 +0100

    replace deprecated colours in link theme

--- packages/@guardian/source-react-components/src/link/theme.ts
+++ packages/@guardian/source-react-components/src/link/theme.ts
@@ -1,4 +1,4 @@
-import { brandAltText, brandText, text } from '@guardian/source-foundations';
+import { palette } from '@guardian/source-foundations';
 
 export type LinkTheme = {
 	textPrimary: string;
@@ -9,23 +9,23 @@ export type LinkTheme = {
 
 export const linkThemeDefault: { link: LinkTheme } = {
 	link: {
-		textPrimary: text.anchorPrimary,
-		textPrimaryHover: text.anchorPrimary,
-		textSecondary: text.anchorSecondary,
-		textSecondaryHover: text.anchorSecondary,
+		textPrimary: palette.brand[500],
+		textPrimaryHover: palette.brand[500],
+		textSecondary: palette.neutral[7],
+		textSecondaryHover: palette.neutral[7],
 	},
 };
 
 export const linkThemeBrand: { link: LinkTheme } = {
 	link: {
-		textPrimary: brandText.anchorPrimary,
-		textPrimaryHover: brandText.anchorPrimary,
+		textPrimary: palette.neutral[100],
+		textPrimaryHover: palette.neutral[100],
 	},
 };
 
 export const linkThemeBrandAlt: { link: LinkTheme } = {
 	link: {
-		textPrimary: brandAltText.anchorPrimary,
-		textPrimaryHover: brandAltText.anchorPrimary,
+		textPrimary: palette.neutral[7],
+		textPrimaryHover: palette.neutral[7],
 	},
 };
