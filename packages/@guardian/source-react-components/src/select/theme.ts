6e5dfdcc39af539c70870e44aef935409bf3b071
tjsilver <thalia.silver@guardian.co.uk>
2022-05-23 16:07:13 +0100

    replace deprecated colours in select theme

--- packages/@guardian/source-react-components/src/select/theme.ts
+++ packages/@guardian/source-react-components/src/select/theme.ts
@@ -1,19 +1,19 @@
-import { background, border, text } from '@guardian/source-foundations';
+import { palette } from '@guardian/source-foundations';
 import { userFeedbackThemeDefault } from '../user-feedback/theme';
 
 export const selectThemeDefault = {
 	select: {
-		textUserInput: text.userInput,
-		textLabel: text.inputLabel,
-		textLabelOptional: text.supporting,
-		textLabelSupporting: text.supporting,
-		textError: text.inputError,
-		textSuccess: text.success,
-		backgroundInput: background.input,
-		border: border.input,
-		borderActive: border.inputActive,
-		borderError: border.error,
-		borderSuccess: border.success,
+		textUserInput: palette.neutral[7],
+		textLabel: palette.neutral[7],
+		textLabelOptional: palette.neutral[46],
+		textLabelSupporting: palette.neutral[46],
+		textError: palette.neutral[7],
+		textSuccess: palette.success[400],
+		backgroundInput: palette.neutral[100],
+		border: palette.neutral[60],
+		borderActive: palette.focus[400],
+		borderError: palette.error[400],
+		borderSuccess: palette.success[400],
 	},
 	...userFeedbackThemeDefault,
 };
