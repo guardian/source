6941e55aef367db52821e1936a34ef74ad34d40c
Oliver Barnwell <barnwelloliver@gmail.com>
2022-08-31 16:57:08 +0100

    deprecate the use of the term 'mapping' when exporting lineHeight, font strings etc, because it is no longer mapping to an array of values, it is just the absolute value for each. shift the fontSizes, remFontSizes and availableFonts mappings to fs.ts as this file was the only consumer

--- packages/@guardian/source-foundations/src/typography/fs.ts
+++ packages/@guardian/source-foundations/src/typography/fs.ts
@@ -1,13 +1,73 @@
 import {
-	availableFonts,
-	fontMapping,
-	fontSizeMapping,
-	fontWeightMapping,
-	lineHeightMapping,
-	remFontSizeMapping,
-	underlineThicknessMapping,
+	bodySizes,
+	fonts,
+	fontWeights,
+	headlineSizes,
+	lineHeights,
+	remBodySizes,
+	remHeadlineSizes,
+	remTextSansSizes,
+	remTitlepieceSizes,
+	textSansSizes,
+	titlepieceSizes,
+	underlineThickness,
 } from './data';
-import type { FontStyle, FontWeightDefinition, Fs, Option } from './types';
+import type {
+	AvailableFontsMapping,
+	FontStyle,
+	FontWeightDefinition,
+	Fs,
+	Option,
+} from './types';
+
+const fontSizes = {
+	titlepiece: titlepieceSizes,
+	headline: headlineSizes,
+	body: bodySizes,
+	textSans: textSansSizes,
+} as const;
+
+const remFontSizes = {
+	titlepiece: remTitlepieceSizes,
+	headline: remHeadlineSizes,
+	body: remBodySizes,
+	textSans: remTextSansSizes,
+} as const;
+
+export const availableFonts = {
+	titlepiece: {
+		bold: {
+			hasItalic: false,
+		},
+	},
+	headline: {
+		light: {
+			hasItalic: true,
+		},
+		medium: {
+			hasItalic: true,
+		},
+		bold: {
+			hasItalic: false,
+		},
+	},
+	body: {
+		regular: {
+			hasItalic: true,
+		},
+		bold: {
+			hasItalic: true,
+		},
+	},
+	textSans: {
+		regular: {
+			hasItalic: true,
+		},
+		bold: {
+			hasItalic: false,
+		},
+	},
+} as AvailableFontsMapping;
 
 function getFontStyle(
 	font: FontWeightDefinition | undefined,
@@ -27,29 +87,26 @@ function getFontStyle(
 export const fs: Fs =
 	(category) =>
 	(level, { lineHeight, fontWeight, fontStyle, unit }) => {
-		const fontFamilyValue = fontMapping[category];
+		const fontFamilyValue = fonts[category];
 		const fontSizeValue: `${number}rem` | number =
 			unit === 'px'
-				? Number(fontSizeMapping[category][level])
+				? Number(fontSizes[category][level])
 				: // @ts-expect-error -- the types actually overlap, see https://gist.github.com/mxdvl/5e31fd5b13670b6a41ddac6c65efeee4
-				  `${Number(remFontSizeMapping[category][level])}rem`;
+				  `${Number(remFontSizes[category][level])}rem`;
 		const lineHeightValue: `${number}px` | number =
 			unit === 'px'
 				? // line-height is defined as a unitless value, so we multiply
 				  // by the element's font-size in px to get the px value
-				  `${
-						lineHeightMapping[lineHeight] *
-						Number(fontSizeMapping[category][level])
-				  }px`
-				: lineHeightMapping[lineHeight];
+				  `${lineHeights[lineHeight] * Number(fontSizes[category][level])}px`
+				: lineHeights[lineHeight];
 		// TODO: consider logging an error in development if a requested
 		// font is unavailable
 		const requestedFont = availableFonts[category][fontWeight];
-		const fontWeightValue = requestedFont ? fontWeightMapping[fontWeight] : '';
+		const fontWeightValue = requestedFont ? fontWeights[fontWeight] : '';
 		const fontStyleValue = getFontStyle(requestedFont, fontStyle);
 		const textDecorationThicknessValue = Number(
 			// @ts-expect-error -- the types actually overlap, see https://gist.github.com/mxdvl/5e31fd5b13670b6a41ddac6c65efeee4
-			underlineThicknessMapping[category][level],
+			underlineThickness[category][level],
 		);
 
 		return Object.assign(
