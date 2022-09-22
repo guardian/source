22dab503cc5f879e4433fd822347672396d48caa
Oliver Barnwell <barnwelloliver@gmail.com>
2022-09-22 16:52:28 +0100

    rename the custom jest matcher registrationf ile

--- packages/@guardian/source-foundations/jest.config.js
+++ packages/@guardian/source-foundations/jest.config.js
@@ -2,5 +2,5 @@
 module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	setupFilesAfterEnv: ['./src/utils/jest-matchers/setup-custom-matchers.ts'],
+	setupFilesAfterEnv: ['./src/utils/jest-matchers/index.ts'],
 };
