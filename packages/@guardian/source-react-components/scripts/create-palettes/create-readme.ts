f591c956e8bdeb5b113db227a98871a50b56d554
Joe Cowton <joe.cowton@guardian.co.uk>
2022-10-03 15:40:33 +0100

    Remove palette creation

--- packages/@guardian/source-react-components/scripts/create-palettes/create-readme.ts
+++
@@ -1,9 +0,0 @@
-export const createReadme = () => `# Palettes
-
-**The contents of this directory are created automatically. Any edits will be
-overwritten sooner or later.**
-
-\`\`\`sh
-yarn workspace @guardian/source-react-components create-palettes
-\`\`\`
-`;
--- packages/@guardian/source-react-components/vendor/palettes/README.md
+++
@@ -1,8 +0,0 @@
-# Palettes
-
-**The contents of this directory are created automatically. Any edits will be
-overwritten sooner or later.**
-
-```sh
-yarn workspace @guardian/source-react-components create-palettes
-```
