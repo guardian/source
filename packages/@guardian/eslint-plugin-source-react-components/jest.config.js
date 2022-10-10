9187ae95b1c88b1b0ed203fe8f5303ab4f10e217
TJ Silver <thalia.silver@guardian.co.uk>
2022-10-10 16:04:40 +0100

    add e2e test to components plugin

---
+++ packages/@guardian/eslint-plugin-source-react-components/jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	setupFilesAfterEnv: ['./lib/jest-matchers/index.ts'],
+};
